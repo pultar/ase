386a3a2cbceaad51cd2b64d089e804f017e858a4
James Kermode <james.kermode@gmail.com>
2016-09-07 12:08:55 +0100

    ENG: new LoggingCalculator to record and plot number of calls to calculator

---
+++ ase/test/loggingcalc.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+from ase.build import bulk
+from ase.calculators.lj import LennardJones
+from ase.calculators.loggingcalc import LoggingCalculator
+from ase.optimize import FIRE, BFGS
+
+a0 = bulk('Cu', cubic=True)
+
+# perturb the atoms
+s = a0.get_scaled_positions()
+# s[:, 0] *= 0.995
+# a0.set_scaled_positions(s)
+a0.rattle(0.05)
+
+log_calc = LoggingCalculator(LennardJones())
+
+for OPT, label in zip([FIRE, BFGS],
+                      ["FIRE", "BFGS"]):
+    log_calc.set_label(label)
+    atoms = a0.copy()
+    atoms.set_calculator(log_calc)
+    opt = OPT(atoms)
+    opt.run(fmax=1e-3)
+
+log_calc.plot([1e-3, 1e2], markers=['r-', 'g-'])
+#import matplotlib.pyplot as plt
+#plt.savefig('logcalc.png')
