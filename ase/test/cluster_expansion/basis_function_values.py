bbba44e1a98a889480aa2dd7a63eb70efbdeb7d3
David Kleiven <davidkleiven446@gmail.com>
2018-03-18 17:46:39 +0100

    Better coding structure

--- ase/test/cluster_expansion/basis_function_values.py
+++ ase/test/cluster_expansion/basis_function_values.py
@@ -1,18 +1,23 @@
-# Verify that the value assigned to elements stays the same
-from ase.ce import BulkCrystal
+"""
+Verify that the value assigned to elements stays the same
+"""
 import os
+from ase.ce import BulkCrystal
 
 def test_binary():
-    db_name="almg_binary_test.db"
+    """
+    Test the basis functions for a binary Al-Mg system
+    """
+    db_name = "almg_binary_test.db"
     conc_args = {
-        "conc_ratio_min_1":[[1,0]],
-        "conc_ratio_max_1":[[0,1]]
+        "conc_ratio_min_1":[[1, 0]],
+        "conc_ratio_max_1":[[0, 1]]
     }
-    bc = BulkCrystal( crystalstructure="fcc", a=4.05, \
-    basis_elements=[["Al","Mg"]], size=[2,2,2], conc_args=conc_args, \
-    db_name=db_name )
+    bc_setting = BulkCrystal(crystalstructure="fcc", a=4.05, \
+    basis_elements=[["Al", "Mg"]], size=[2, 2, 2], conc_args=conc_args, \
+    db_name=db_name)
 
-    basis_funcs = bc.basis_functions[0]
+    basis_funcs = bc_setting.basis_functions[0]
 
     expected_values = {
         "Al":1.0,
@@ -21,7 +26,7 @@ def test_binary():
 
     tol = 1E-6
     for key in expected_values.keys():
-        assert( abs(expected_values[key]-basis_funcs[key]) < tol )
+        assert abs(expected_values[key]-basis_funcs[key]) < tol
 
     # Delete the database
     os.remove(db_name)
--- ase/test/cluster_expansion/run_ce.py
+++ ase/test/cluster_expansion/run_ce.py
@@ -1,8 +1,10 @@
-# Test:
-# 1. Initialize the CE
-# 2. Add a few structures
-# 3. Compute the energy
-# 4. Run the evaluation routine
+"""
+Test:
+1. Initialize the CE
+2. Add a few structures
+3. Compute the energy
+4. Run the evaluation routine
+"""
 
 import os
 from ase.ce import BulkCrystal
@@ -13,38 +15,42 @@ from ase.db import connect
 
 
 def test_binary_system():
-    db_name="aucu_binary_test.db"
+    """
+    Verifies that one can run a CE for the binary Au-Cu system.
+    The EMT calculator is used for energy calculations
+    """
+    db_name = "aucu_binary_test.db"
     conc_args = {
-        "conc_ratio_min_1":[[1,0]],
-        "conc_ratio_max_1":[[0,1]]
+        "conc_ratio_min_1":[[1, 0]],
+        "conc_ratio_max_1":[[0, 1]]
     }
-    bc = BulkCrystal( crystalstructure="fcc", a=4.05, \
-    basis_elements=[["Au","Cu"]], size=[3,3,3], conc_args=conc_args, \
-    db_name=db_name )
+    bc_setting = BulkCrystal(crystalstructure="fcc", a=4.05, \
+    basis_elements=[["Au", "Cu"]], size=[3, 3, 3], conc_args=conc_args, \
+    db_name=db_name)
 
-    struct_generator = GenerateStructures( bc, struct_per_gen=3 )
+    struct_generator = GenerateStructures(bc_setting, struct_per_gen=3)
     struct_generator.generate_initial_pool()
 
     # Compute the energy of the structures
     calc = EMT()
-    db = connect( db_name )
+    database = connect(db_name)
     all_atoms = []
     key_value_pairs = []
-    for row in db.select("converged=0"):
+    for row in database.select("converged=0"):
         atoms = row.toatoms()
         all_atoms.append(atoms)
-        key_value_pairs.append( row.key_value_pairs )
+        key_value_pairs.append(row.key_value_pairs)
 
     # Write the atoms to the database
-    for atoms,kvp in zip(all_atoms,key_value_pairs):
+    for atoms, kvp in zip(all_atoms, key_value_pairs):
         atoms.set_calculator(calc)
         atoms.get_potential_energy()
         kvp["converged"] = True
-        db.write( atoms, key_value_pairs=kvp )
+        database.write(atoms, key_value_pairs=kvp)
 
     # Evaluate
-    evaluator = Evaluate( bc, penalty="l2", lamb=1E-6 )
+    evaluator = Evaluate(bc_setting, penalty="l2", lamb=1E-6)
     evaluator.get_cluster_name_eci_dict
-    os.remove( db_name )
+    os.remove(db_name)
 
 test_binary_system()
