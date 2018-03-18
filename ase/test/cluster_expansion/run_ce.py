57e39c7bcf1dffeff930bc4124afb648aefe4f28
David Kleiven <davidkleiven446@gmail.com>
2018-03-18 15:38:17 +0100

    Test that verifies that one can run a simple CE for a binary system

---
+++ ase/test/cluster_expansion/run_ce.py
@@ -0,0 +1,52 @@
+# Test:
+# 1. Initialize the CE
+# 2. Add a few structures
+# 3. Compute the energy
+# 4. Run the evaluation routine
+
+import os
+from ase.ce import BulkCrystal
+from ase.ce import GenerateStructures
+from ase.ce import Evaluate
+from ase.calculators.emt import EMT # Use this calculator as it is fast
+from ase.db import connect
+import os
+
+
+def test_binary_system():
+    db_name="aucu_binary_test.db"
+    conc_args = {
+        "conc_ratio_min_1":[[1,0]],
+        "conc_ratio_max_1":[[0,1]]
+    }
+    bc = BulkCrystal( crystalstructure="fcc", a=4.05, \
+    basis_elements=[["Au","Cu"]], size=[3,3,3], conc_args=conc_args, \
+    db_name=db_name )
+    #bc._get_cluster_information()
+
+    struct_generator = GenerateStructures( bc, struct_per_gen=3 )
+    struct_generator.generate_initial_pool()
+
+    # Compute the energy of the structures
+    calc = EMT()
+    db = connect( db_name )
+    all_atoms = []
+    key_value_pairs = []
+    for row in db.select("converged=0"):
+        atoms = row.toatoms()
+        all_atoms.append(atoms)
+        key_value_pairs.append( row.key_value_pairs )
+
+    # Write the atoms to the database
+    for atoms,kvp in zip(all_atoms,key_value_pairs):
+        atoms.set_calculator(calc)
+        energy = atoms.get_potential_energy()
+        kvp["converged"] = True
+        db.write( atoms, key_value_pairs=kvp )
+
+    # Evaluate
+    evaluator = Evaluate( bc, penalty="l2", lamb=1E-6 )
+    ecis = evaluator.get_cluster_name_eci_dict
+    os.remove( db_name )
+
+test_binary_system()
