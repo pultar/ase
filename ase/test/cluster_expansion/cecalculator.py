a49570d0e3af8dfee36ea13b5f050b0980dc8cda
David Kleiven <david.kleiven@ntnu.no>
2018-05-30 10:23:01 +0200

    CE calculator test

---
+++ ase/test/cluster_expansion/cecalculator.py
@@ -0,0 +1,54 @@
+from ase.calculators.cluster_expansion import ClusterExpansion
+from ase.ce import BulkCrystal
+from ase.ce import CorrFunction
+from random import shuffle
+
+def generate_ex_eci(bc):
+    cf = CorrFunction(bc)
+    cf = cf.get_cf(bc.atoms)
+    eci = {key:-0.001 for key in cf}
+    return eci
+
+def all_cf_match(cf_dict, calc):
+    tol = 1E-4
+    for i,name in enumerate(calc.cluster_names):
+        print (cf_dict[name],calc.cf[i])
+        assert abs(cf_dict[name]-calc.cf[i]) < tol
+
+def test_update_correlation_functions(n_trial_configs=20):
+    db_name = "aucu_binary_test.db"
+    conc_args = {
+        "conc_ratio_min_1":[[1, 0]],
+        "conc_ratio_max_1":[[0, 1]]
+    }
+    bc_setting = BulkCrystal(crystalstructure="fcc", a=4.05, \
+    basis_elements=[["Au", "Cu"]], size=[3, 3, 3], conc_args=conc_args, \
+    db_name=db_name)
+    cf = CorrFunction(bc_setting)
+    atoms = bc_setting.atoms
+
+    eci = generate_ex_eci(bc_setting)
+
+    # Insert some Au and Cu elements
+    for i in range(int(len(atoms)/2)):
+        atoms[i].symbol = "Au"
+        atoms[-i-1].symbol = "Cu"
+    init_cf = cf.get_cf(atoms)
+
+    calc = ClusterExpansion(bc_setting, cluster_name_eci=eci, init_cf=init_cf)
+    atoms.set_calculator(calc)
+
+    indices = range(len(atoms))
+    symbols = [atom.symbol for atom in atoms]
+    for _ in range(n_trial_configs):
+        shuffle(symbols)
+        for i,symb in enumerate(symbols):
+            atoms[i].symbol = symb
+
+        # The calculator should update its correlation functions
+        # when the energy is computed
+        energy = atoms.get_potential_energy()
+        brute_force_cf = cf.get_cf(atoms)
+        all_cf_match(brute_force_cf, calc)
+
+test_update_correlation_functions(n_trial_configs=20)
--- ase/test/cluster_expansion/test_structure_comparator.py
+++ ase/test/cluster_expansion/test_structure_comparator.py
@@ -2,6 +2,8 @@ from ase.ce import StructureComparator
 import glob
 from ase.io import read
 import os
+from ase.test import NotAvailable
+import ase
 
 """
 Inside the folder structure_match_data all structures inside the same subfolder
@@ -9,15 +11,14 @@ should be symmetrically equivalent so the comparator should return True
 when comparing these two atoms
 
 When comparing atoms-objects from different subfolders they should return False
-
-If this test is run as a standalond script run
-
-export CI_PROJECT_DIR=/path/to/ase/
-
-first
 """
 
-test_fname_ref = os.environ["CI_PROJECT_DIR"]+"/ase/test/cluster_expansion/structure_match_data/equiv331_"
+ase_folder = ase.__file__.rpartition("/")[0]
+folder = ase_folder+"/test/cluster_expansion/structure_match_data"
+if not os.path.exists(folder):
+    raise NotAvailable("Cannot locate the datafiles for the test structures")
+
+test_fname_ref = root_folder+"/equiv331_"
 n_sets = 218
 cpp_stepsize = 1
 python_stepsize = 20
@@ -26,7 +27,7 @@ verbose = False
 
 def test_equal(comparator,step):
     # All structure in each folder are similar
-    for i in range(0,n_sets,step):
+    for i in range(0, n_sets, step):
         ref_file = test_fname_ref+"{}/equiv331_{}_0.xyz".format(i,i)
         ref_atoms = read(ref_file)
         for struct_file in glob.glob(test_fname_ref+"{}/*.xyz".format(i)):
@@ -37,10 +38,10 @@ def test_not_equal(comparator,step):
     # Test only a selection of possible combinations
     comparator = StructureComparator()
     for i in range(0,n_sets,step):
-        ref_file = test_fname_ref+"{}/equiv331_{}_0.xyz".format(i,i)
+        ref_file = test_fname_ref+"{}/equiv331_{}_0.xyz".format(i, i)
         atoms1 = read(ref_file)
         for j in range(i+1,n_sets,false_structure_stepsize):
-            ref_file2 = test_fname_ref+"{}/equiv331_{}_0.xyz".format(j,j)
+            ref_file2 = test_fname_ref+"{}/equiv331_{}_0.xyz".format(j, j)
             atoms2 = read(ref_file2)
             assert not comparator.compare(atoms1,atoms2)
 
