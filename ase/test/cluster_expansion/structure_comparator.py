1c5bd596c530b47b4476986a28fe7587ffcf1967
David Kleiven <david.kleiven@ntnu.no>
2018-05-30 11:04:31 +0200

    Remove test from the filename

--- ase/test/cluster_expansion/structure_comparator.py
+++ ase/test/cluster_expansion/structure_comparator.py
@@ -13,8 +13,13 @@ when comparing these two atoms
 When comparing atoms-objects from different subfolders they should return False
 """
 
-ase_folder = ase.__file__.rpartition("/")[0]
-folder = ase_folder + "/test/cluster_expansion/structure_match_data"
+if ( "CI_PROJECT_DIR" in os.environ.keys() ):
+    # This is the path to the test data on the ASE test server
+    folder = os.environ["CI_PROJECT_DIR"]+"/ase/test/cluster_expansion/structure_match_data"
+else:
+    # This is the path when running locally
+    ase_folder = ase.__file__.rpartition("/")[0]
+    folder = ase_folder + "/test/cluster_expansion/structure_match_data"
 if not os.path.exists(folder):
     raise NotAvailable("Cannot locate the datafiles for the test structures")
 
