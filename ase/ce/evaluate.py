3643b683bbd8d0a3d87aca8ea1a500f1b3f36bfd
Jin Chang <jchang@dtu.dk>
2018-05-31 12:04:57 +0200

    changed evaluate.py

--- ase/ce/evaluate.py
+++ ase/ce/evaluate.py
@@ -17,21 +17,24 @@ class Evaluate(object):
     """Class that evaluates RMSE and CV scores. It also generates a plot that
     compares the energies predicted by CE and that are obtained from DFT
     calculations.
+
+    Arguments:
+    =========
+    setting: BulkCrystal or BulkSapcegroup object
+    cluster_names: list
+        Names of clusters to include in the evalutation.
+        If None, all of the possible clusters are included.
+    select_cond: tuple or list of tuples
+        Extra selection condition specified by user.
+        Default only includes "converged=True".
+    penalty: str
+        Type of regularization to be used.
+        -*None*: no regularization
+        -'lasso' or 'l1': L1 regularization
+        -'euclidean' or 'l2': L2 regularization
     """
-    def __init__(self, setting, cluster_names=None, select_cond=None, lamb=0.0,
+    def __init__(self, setting, cluster_names=None, select_cond=None,
                  penalty=None):
-        """
-        setting: BulkCrystal object
-        cluster_names: names of clusters to include in the evalutation.
-                       If None, all of the possible clusters are included.
-        select_cond: extra selection condition specified by user. Default only
-                     includes "converged=True".
-        lamb: regression coefficient lambda
-        penalty: type of regularization (compressed sensing).
-                 None - no regularization
-                 'lasso' or 'l1' - L1 regularization
-                 'euclidean' or 'l2' - L2 regularization
-        """
         if not isinstance(setting, (BulkCrystal, BulkSpacegroup)):
             raise TypeError("setting must be BulkCrystal or BulkSpacegroup "
                             "object")
@@ -42,14 +45,10 @@ class Evaluate(object):
         # Define the selection conditions
         self.select_cond = [('converged', '=', True)]
         if select_cond is not None:
-            for cond in select_cond:
-                self.select_cond.append(cond)
-
-        # Check the regression coefficient lambda
-        if type(lamb) is not int and type(lamb) is not float:
-            raise TypeError("The penalty coefficient 'lamb' must be either"
-                            " int or float type.")
-        self.lamb = float(lamb)
+            if isinstance(select_cond, list):
+                self.select_cond += select_cond
+            else:
+                self.select_cond.append(select_cond)
 
         # Determine the type of penalty for compressed sensing
         if penalty is None or penalty is False:
@@ -71,9 +70,22 @@ class Evaluate(object):
         else:
             self.cluster_names = cluster_names
         self.cf_matrix = self._make_cf_matrix()
+        self.e_dft = self._get_dft_energy_per_atom()
         self.eci = None
-        self.e_dft = None
-        self.e_pred = None
+        self.alpha = None
+        self.e_pred_loo = None
+
+    @property
+    def full_cf_matrix(self):
+        """Get correlation function of every entry.
+        This method is used for evaluating the variance when creating probe
+        structures.
+        """
+        cfm = []
+        for row in self.setting.db.select([('name', '!=', 'information')]):
+            cfm.append([row[x] for x in self.cluster_names])
+        cfm = np.array(cfm, dtype=float)
+        return cfm
 
     def _make_cf_matrix(self):
         """
@@ -105,13 +117,28 @@ class Evaluate(object):
         db = connect(self.setting.db_name)
         for row in db.select(self.select_cond):
             e_dft.append(row.energy / row.natoms)
-        self.e_dft = np.array(e_dft)
-        return True
+        return np.array(e_dft)
 
-    @property
-    def get_eci(self):
-        if self.e_dft is None:
-            self._get_dft_energy_per_atom()
+    def get_eci(self, alpha):
+        """Determine and return ECIs for a given alpha.
+
+        This method also saves the last value of alpha used (self.alpha) and
+        the corresponding ECIs (self.eci) such that ECIs are not calculated
+        repeated if alpha value is unchanged.
+
+        Argument:
+        ========
+        alpha: int or float
+            regression coefficient.
+
+
+        """
+
+        # Check the regression coefficient alpha
+        if not isinstance(alpha, (int, float)):
+            raise TypeError("The penalty coefficient 'lamb' must be either"
+                            " int or float type.")
+        self.alpha = float(alpha)
 
         n_col = self.cf_matrix.shape[1]
 
@@ -130,92 +157,68 @@ class Evaluate(object):
             identity = np.identity(n_col)
             identity[0][0] = 0.
             a = inv(self.cf_matrix.T.dot(self.cf_matrix) +
-                    self.lamb * identity).dot(self.cf_matrix.T)
+                    alpha * identity).dot(self.cf_matrix.T)
             eci = a.dot(self.e_dft)
 
         elif self.penalty == 'l1':
-            lasso = Lasso(alpha=self.lamb, fit_intercept=False, copy_X=True,
-                          normalize=True, max_iter=1e5)
+            lasso = Lasso(alpha=alpha, fit_intercept=False, copy_X=True,
+                          normalize=True, max_iter=1e6)
             lasso.fit(self.cf_matrix, self.e_dft)
             eci = lasso.coef_
             print('Number of nonzero ECIs: {}'.format(len(np.nonzero(eci)[0])))
 
         else:
-            raise TypeError("Unknown penalty type.")
+            raise ValueError("Unknown penalty type.")
 
         self.eci = eci
         return eci
 
-    @property
-    def get_cluster_name_eci_tuple(self):
-        if self.eci is None:
-            self.get_eci
+    def get_cluster_name_eci(self, alpha, return_type='tuple'):
+        """Determine and return the cluster names and their corresponding ECI
+        values for the provided alpha (regression coefficient) value.
+
+
+        """
+        if float(alpha) != self.alpha:
+            self.get_eci(alpha)
         # sanity check
         if len(self.cluster_names) != len(self.eci):
             raise ValueError('lengths of cluster_names and ECIs are not same')
 
         i_nonzeros = np.nonzero(self.eci)[0]
-        tuples = []
+        pairs = []
         for i, cname in enumerate(self.cluster_names):
             if i not in i_nonzeros:
                 continue
-            tuples.append((cname, self.eci[i]))
-        return tuples
+            pairs.append((cname, self.eci[i]))
 
-    @property
-    def get_cluster_name_eci_dict(self):
-        if self.eci is None:
-            self.get_eci
-        # sanity check
-        if len(self.cluster_names) != len(self.eci):
-            raise ValueError('lengths of cluster_names and ECIs are not same')
+        if type == 'dict':
+            return dict(pairs)
+        return pairs
 
-        i_nonzeros = np.nonzero(self.eci)[0]
-        dict = {}
-        for i, cname in enumerate(self.cluster_names):
-            if i not in i_nonzeros:
-                continue
-            dict[cname] = self.eci[i]
-        return dict
+    def plot_energy(self, alpha):
+        if float(alpha) != self.alpha:
+            self.get_eci(alpha)
+        e_pred = self.cf_matrix.dot(self.eci)
 
-    def _get_e_predict(self):
-        """
-        Energy predicted by the CE model.
-        E_{predicted} = cf_matrix * eci
-        """
-        if self.eci is None:
-            self.get_eci
-        self.e_pred = self.cf_matrix.dot(self.eci)
-        return True
-
-    def plot_energy(self):
-        if self.e_pred is None:
-            self._get_e_predict()
-
-        rmin = min(np.append(self.e_dft, self.e_pred)) - 0.1
-        rmax = max(np.append(self.e_dft, self.e_pred)) + 0.1
+        rmin = min(np.append(self.e_dft, e_pred)) - 0.1
+        rmax = max(np.append(self.e_dft, e_pred)) + 0.1
 
         t = np.arange(rmin - 10, rmax + 10, 1)
         fig = plt.figure()
         ax = fig.add_subplot(111)
         ax.set_title('Fit using {} data points'.format(self.e_dft.shape[0]))
-        ax.plot(self.e_pred, self.e_dft, 'bo', mfc='none')
+        ax.plot(e_pred, self.e_dft, 'bo', mfc='none')
         ax.plot(t, t, 'r')
         ax.axis([rmin, rmax, rmin, rmax])
         ax.set_ylabel(r'$E_{DFT}$ (eV/atom)')
         ax.set_xlabel(r'$E_{pred}$ (eV/atom)')
-        ax.text(0.95, 0.01, 'CV = %.3f meV/atom \n RMSE = %.3f meV/atom'
-                %(self._cv_loo()*1000, self.rmse()*1000),
-                  verticalalignment='bottom',
-                  horizontalalignment='right',
-                  transform=ax.transAxes,
-                  fontsize=12)
-        # ax.text(0.95, 0.01, 'avg. prediction error = %.3f meV/atom '
-        #         %(self._cv_loo()*1000),
-        #           verticalalignment='bottom',
-        #           horizontalalignment='right',
-        #           transform=ax.transAxes,
-        #           fontsize=12)
+        ax.text(0.95, 0.01,
+                "CV = {0:.3f} meV/atom \n"
+                "RMSE = {1:.3f} meV/atom".format(self._cv_loo(alpha) * 1000,
+                                                 self.rmse(alpha) * 1000),
+                verticalalignment='bottom', horizontalalignment='right',
+                transform=ax.transAxes, fontsize=12)
         ax.plot(self.e_pred_loo, self.e_dft, 'ro', mfc='none')
         plt.show()
         return True
@@ -228,7 +231,6 @@ class Evaluate(object):
         returning the reduced values.
         """
         cname_list = deepcopy(self.cluster_names)
-        # cfm = cfm[:, ~np.all(cfm == 0., axis=0)]
         offset = 0
         rank = matrix_rank(cfm)
         while cfm.shape[1] > rank:
@@ -245,14 +247,12 @@ class Evaluate(object):
         self.cluster_names = cname_list
         return True
 
-    def _get_eci_loo(self, i):
+    def _get_eci_loo(self, i, alpha):
         """
         Determines the ECIs for Leave-one-out case. Eliminates the ith row of
         the cf_matrix when determining the ECIs.
         Returns the determined ECIs.
         """
-        if self.e_dft is None:
-            self._get_dft_energy_per_atom()
         n_col = self.cf_matrix.shape[1]
         cfm = np.delete(self.cf_matrix, i, 0)
         e_dft = np.delete(self.e_dft, i, 0)
@@ -262,10 +262,10 @@ class Evaluate(object):
         elif self.penalty == 'l2':
             identity = np.identity(n_col)
             identity[0][0] = 0.
-            a = inv(cfm.T.dot(cfm) + self.lamb * identity).dot(cfm.T)
+            a = inv(cfm.T.dot(cfm) + alpha * identity).dot(cfm.T)
             eci = a.dot(e_dft)
         elif self.penalty == 'l1':
-            lasso = Lasso(alpha=self.lamb, fit_intercept=False, copy_X=True,
+            lasso = Lasso(alpha=alpha, fit_intercept=False, copy_X=True,
                           normalize=True, max_iter=1e5)
             lasso.fit(cfm, e_dft)
             eci = lasso.coef_
@@ -273,14 +273,14 @@ class Evaluate(object):
             raise TypeError("Unknown penalty type.")
         return eci
 
-    def _cv_loo(self):
-        """
-        Determines the CV score for the Leave-One-Out case.
+    def _cv_loo(self, alpha):
+        """Determines the CV score for the Leave-One-Out case.
+
         """
         cv_sq = 0.
         e_pred_loo = []
         for i in range(self.cf_matrix.shape[0]):
-            eci = self._get_eci_loo(i)
+            eci = self._get_eci_loo(i, alpha)
             e_pred = self.cf_matrix[i][:].dot(eci)
             delta_e = self.e_dft[i] - e_pred
             cv_sq += (delta_e)**2
@@ -289,17 +289,19 @@ class Evaluate(object):
         self.e_pred_loo = e_pred_loo
         return np.sqrt(cv_sq)
 
-    def __cv_loo_fast(self):
-        """
-        Calculate cross-validation (CV) score of the model based on the method
-        presented in J. Phase Equilib. 23, 348 (2002).
+    def __cv_loo_fast(self, alpha):
+        """Calculate cross-validation (CV) score of the model based on the
+        method presented in J. Phase Equilib. 23, 348 (2002).
         This method has a computational complexity of order n^1.
         """
         # For each structure i, predict energy based on the ECIs determined
         # using (N-1) structures and the parameters corresponding to the
         # structure i.
         # CV^2 = N^{-1} * Sum((E_DFT-E_pred) / (1 - X_i (X^T X)^{-1} X_u^T))^2
-        delta_e = self.e_dft - self.e_pred
+        if float(alpha) != self.alpha:
+            self.get_eci(alpha)
+        e_pred = self.cf_matrix.dot(self.eci)
+        delta_e = self.e_dft - e_pred
         cfm = self.cf_matrix
         # precision matrix
         prec = inv(cfm.T.dot(cfm))
@@ -309,18 +311,28 @@ class Evaluate(object):
         cv_sq /= cfm.shape[0]
         return np.sqrt(cv_sq)
 
-    def mae(self):
-        if self.e_pred is None:
-            self._get_e_predict()
-        delta_e = self.e_dft - self.e_pred
+    def mae(self, alpha):
+        if float(alpha) != self.alpha:
+            self.get_eci(alpha)
+        e_pred = self.cf_matrix.dot(self.eci)
+        delta_e = self.e_dft - e_pred
         return sum(np.absolute(delta_e)) / len(delta_e)
 
-    def rmse(self):
-        if self.e_pred is None:
-            self._get_e_predict()
-        delta_e = self.e_dft - self.e_pred
+    def rmse(self, alpha):
+        """Generate a RMSE of the fit.
+
+        Arguments:
+        =========
+        alpha: int or float
+            regression coefficient.
+        """
+        if float(alpha) != self.alpha:
+            self.get_eci(alpha)
+        e_pred = self.cf_matrix.dot(self.eci)
+        delta_e = self.e_dft - e_pred
+        num_entries = len(delta_e)
         rmse_sq = 0.0
-        for i in range(len(delta_e)):
+        for i in range(num_entries):
             rmse_sq += (delta_e[i])**2
-        rmse_sq /= len(delta_e)
+        rmse_sq /= num_entries
         return np.sqrt(rmse_sq)
