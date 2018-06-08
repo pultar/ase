9e7b5cb1a9dd1c26400f5af58b444d36750abe69
Jin Chang <jchang@dtu.dk>
2018-06-08 15:01:17 +0200

    added a new test

--- ase/test/cluster_expansion/bulkspacegroup.py
+++ ase/test/cluster_expansion/bulkspacegroup.py
@@ -24,32 +24,50 @@ def test_spgroup_217():
     basis_elements = [["Al", "Mg"], ["Al", "Mg"], ["Al", "Mg"], ["Al", "Mg"]]
 
     # Test with grouped basis
-    bs = BulkSpacegroup(basis_elements=basis_elements,
-                        basis=basis,
-                        spacegroup=217,
-                        cellpar=cellpar,
-                        conc_args=conc_args,
-                        max_cluster_size=4,
-                        db_name=db_name,
-                        size=[1, 1, 1],
-                        grouped_basis=[[0, 1, 2, 3]],
-                        max_cluster_dist=5.0)
+    bsg = BulkSpacegroup(basis_elements=basis_elements,
+                         basis=basis,
+                         spacegroup=217,
+                         cellpar=cellpar,
+                         conc_args=conc_args,
+                         max_cluster_size=4,
+                         db_name=db_name,
+                         size=[1, 1, 1],
+                         grouped_basis=[[0, 1, 2, 3]],
+                         max_cluster_dist=5.0)
     os.remove(db_name)
 
     conc_args = {"conc_ratio_min_1": [[1, 0], [1, 0], [1, 0], [1, 0]],
                  "conc_ratio_max_1": [[0, 1], [0, 1], [0, 1], [0, 1]]}
     basis_elements = [["Al", "Mg"], ["Si", "Mg"], ["Cu", "Mg"], ["Zn", "Mg"]]
     # Test without grouped basis
-    bs = BulkSpacegroup(basis_elements=basis_elements,
-                        basis=basis,
-                        spacegroup=217,
-                        cellpar=cellpar,
-                        conc_args=conc_args,
-                        max_cluster_size=4,
-                        db_name=db_name,
-                        size=[1, 1, 1],
-                        max_cluster_dist=5.0)
+    bsg = BulkSpacegroup(basis_elements=basis_elements,
+                         basis=basis,
+                         spacegroup=217,
+                         cellpar=cellpar,
+                         conc_args=conc_args,
+                         max_cluster_size=4,
+                         db_name=db_name,
+                         size=[1, 1, 1],
+                         max_cluster_dist=5.0)
     os.remove(db_name)
+
+def test_grouped_basis_with_large_dist():
+    # Test with grouped basis
+    bsg = BulkSpacegroup(basis_elements=[['Li', 'X', 'V'], ['Li', 'X', 'V'],
+                                         ['O', 'F']],
+                         basis=[(0.000000, 0.000000, 0.000000),
+                                (0.333333, 0.666667, 0.000000),
+                                (0.333333, 0.000000, 0.250000)],
+                         spacegroup=167,
+                         cellpar=[5.123, 5.123, 13.005, 90., 90., 120.],
+                         conc_args={"conc_ratio_min_1": [[0, 2, 1], [2, 1]],
+                                    "conc_ratio_max_1": [[2, 0, 1], [2, 1]]},
+                         db_name="test.db",
+                         size=[1, 1, 1],
+                         grouped_basis=[[0, 1], [2]],
+                         max_cluster_dist=5.0)
+    os.remove("test.db")
 
 
 test_spgroup_217()
+test_grouped_basis_with_large_dist()
