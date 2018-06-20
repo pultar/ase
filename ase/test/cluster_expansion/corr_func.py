fed9dd712737ce1bb2302b2a3f3fe49628379ad8
David Kleiven <david.kleiven@ntnu.no>
2018-06-20 08:19:35 +0200

    Unit test for correlation functions

---
+++ ase/test/cluster_expansion/corr_func.py
@@ -0,0 +1,67 @@
+
+import os
+from ase.ce import BulkCrystal, CorrFunction
+from ase.visualize import view
+
+db_name = "test.db"
+conc_args = {"conc_ratio_min_1": [[1, 0]],
+             "conc_ratio_max_1": [[0, 1]]}
+bc_setting = BulkCrystal(crystalstructure="fcc", a=4.05,
+                         basis_elements=[["Au", "Cu", "Si"]], size=[4, 4, 4],
+                         conc_args=conc_args, db_name=db_name)
+
+def test_trans_matrix():
+    """Check that the MIC distance between atoms are correct"""
+    atoms = bc_setting.atoms
+    tm = bc_setting.trans_matrix
+    ref_dist = atoms.get_distance(0, 1, mic=True)
+    for indx in range(len(atoms)):
+        dist = atoms.get_distance(indx, tm[indx,1], mic=True)
+        assert abs(dist-ref_dist) < 1E-5
+
+def get_mic_dists(atoms, cluster):
+    dists = []
+    for indx in cluster:
+        dist = atoms.get_distances(indx, cluster, mic=True)
+        dists.append(dist)
+    return dists
+def test_order_indep_ref_indx():
+    """Check that the order of the elements are independent of the reference index"""
+
+    # Find a 4-body cluster with only in-equivalent sites for this test
+    for i in range(0,len(bc_setting.cluster_indx[0][4])):
+        if not bc_setting.cluster_eq_sites[0][4][i]:
+            cluster = bc_setting.cluster_indx[0][4][i]
+            cluster_order = bc_setting.cluster_order[0][4][i]
+            print bc_setting.cluster_eq_sites[0][4][i]
+            print bc_setting.cluster_names[0][4][i]
+            break
+
+    init_cluster = [0]+cluster[0]
+    init_cluster = [init_cluster[indx] for indx in cluster_order[0]]
+    bc_setting.view_clusters()
+    atoms = bc_setting.atoms
+    print(get_mic_dists(atoms, init_cluster))
+    print("==")
+
+    # Make sure that when the other indices in init_cluster are reference
+    # indices, the order is the same
+    for ref_indx in cluster[0]:
+        found_cluster = False
+        for subcluster, order in zip(cluster, cluster_order):
+            new_cluster = [ref_indx]
+            for indx in subcluster:
+                trans_indx = bc_setting.trans_matrix[ref_indx, indx]
+                new_cluster.append(trans_indx)
+
+            # Check if all elements are the same
+            if sorted(new_cluster) == sorted(init_cluster):
+                new_cluster = [new_cluster[indx] for indx in order]
+                print get_mic_dists(atoms, new_cluster)
+                print("==")
+                found_cluster = True
+                assert init_cluster == new_cluster
+        assert found_cluster
+
+test_trans_matrix()
+test_order_indep_ref_indx()
