456ac9ab7e0d2a7f96bfa00b0771e5f793c49364
David Kleiven <david.kleiven@ntnu.no>
2018-06-21 12:12:09 +0200

    Clean up

--- ase/ce/corrFunc.py
+++ ase/ce/corrFunc.py
@@ -264,14 +264,8 @@ class CorrFunction(object):
             count += temp_cnt
         return sp, count
 
-    def _spin_product_one_cluster(
-            self,
-            ref_indx,
-            atoms,
-            cluster_indices,
-            order,
-            eq_sites,
-            deco):
+    def _spin_product_one_cluster(self, ref_indx, atoms, cluster_indices, order, \
+            eq_sites, deco):
         """Compute the spin product for one cluster category"""
         bf = self.setting.basis_functions
         count = 0
