f019d18b25eb102bf0fd2f16241b6e97a1c739f8
David Kleiven <david.kleiven@ntnu.no>
2018-07-10 10:20:51 +0200

    PEP8

--- ase/test/cluster_expansion/corr_func.py
+++ ase/test/cluster_expansion/corr_func.py
@@ -1,4 +1,4 @@
-
+"""Unit tests for the corr function class."""
 import os
 from ase.ce import BulkCrystal, CorrFunction
 from ase.ce.corrFunc import equivalent_deco
@@ -12,7 +12,7 @@ bc_setting = BulkCrystal(crystalstructure="fcc", a=4.05,
 
 
 def test_trans_matrix():
-    """Check that the MIC distance between atoms are correct"""
+    """Check that the MIC distance between atoms are correct."""
     atoms = bc_setting.atoms
     tm = bc_setting.trans_matrix
     ref_dist = atoms.get_distance(0, 1, mic=True)
@@ -22,6 +22,7 @@ def test_trans_matrix():
 
 
 def get_mic_dists(atoms, cluster):
+    """Get the MIC dist."""
     dists = []
     for indx in cluster:
         dist = atoms.get_distances(indx, cluster, mic=True)
@@ -30,10 +31,11 @@ def get_mic_dists(atoms, cluster):
 
 
 def test_order_indep_ref_indx():
-    """Check that the order of the elements are independent of the
-    reference index. This does only apply for clusters with only inequivalent
-    sites"""
+    """Check that the order of the elements are independent of the ref index.
 
+    This does only apply for clusters with only inequivalent
+    sites
+    """
     for size in range(3, len(bc_setting.cluster_indx[0])):
         for i in range(0, len(bc_setting.cluster_indx[0][size])):
             if bc_setting.cluster_eq_sites[0][size][i]:
@@ -45,7 +47,6 @@ def test_order_indep_ref_indx():
 
             init_cluster = [0] + cluster[0]
             init_cluster = [init_cluster[indx] for indx in cluster_order[0]]
-            atoms = bc_setting.atoms
 
             # Make sure that when the other indices in init_cluster are ref
             # indices, the order is the same
@@ -66,9 +67,12 @@ def test_order_indep_ref_indx():
 
 
 def test_interaction_contribution_symmetric_clusters():
-    """Test that when one atom is surrounded by equal atoms,
+    """Test contribution from symmetric clusters.
+
+    Test that when one atom is surrounded by equal atoms,
     the contribution from all clusters within one category is
-    the same"""
+    the same.
+    """
     from ase.build import bulk
     from ase.ce.tools import wrap_and_sort_by_position
 
