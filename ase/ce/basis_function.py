6ce049ac342d3f9a0a5421ca1a29dd9a4781e9b4
Jin Chang <jchang@dtu.dk>
2018-07-13 18:58:26 +0200

    changed coefficient definition for spin_dict

--- ase/ce/basis_function.py
+++ ase/ce/basis_function.py
@@ -54,40 +54,48 @@ class Sanchez(BasisFunction):
 
     def _get_basis_functions(self):
         """Create basis functions to guarantee the orthonormality."""
-        if self.num_unique_elements == 2:
-            d0_0 = 1.
-        elif self.num_unique_elements == 3:
-            d0_0 = np.sqrt(3. / 2)
-            c0_1 = np.sqrt(2)
-            c1_1 = -3 / np.sqrt(2)
-        elif self.num_unique_elements == 4:
-            d0_0 = np.sqrt(2. / 5)
-            c0_1 = -5. / 3
-            c1_1 = 2. / 3
-            d0_1 = -17. / (3 * np.sqrt(10))
-            d1_1 = np.sqrt(5. / 2) / 3
-        elif self.num_unique_elements == 5:
-            d0_0 = 1. / np.sqrt(2)
-            c0_1 = -1 * np.sqrt(10. / 7)
-            c1_1 = np.sqrt(5. / 14)
-            d0_1 = -17. / (6 * np.sqrt(2))
-            d1_1 = 5. / (6 * np.sqrt(2))
-            c0_2 = 3 * np.sqrt(2. / 7)
-            c1_2 = -155. / (12 * np.sqrt(14))
-            c2_2 = 15 * np.sqrt(7. / 2) / 12
-        elif self.num_unique_elements == 6:
-            d0_0 = np.sqrt(3. / 14)
-            c0_1 = -np.sqrt(2)
-            c1_1 = 3. / (7 * np.sqrt(2))
-            d0_1 = -7. / 6
-            d1_1 = 1. / 6
-            c0_2 = 9 * np.sqrt(3. / 2) / 5
-            c1_2 = -101. / (28 * np.sqrt(6))
-            c2_2 = 7. / (20 * np.sqrt(6))
-            d0_2 = 131. / (15 * np.sqrt(4))
-            d1_2 = -7 * np.sqrt(7. / 2) / 12
-            d2_2 = np.sqrt(7. / 2) / 20
-        else:
+        # d0_0 = np.array([1.0, np.sqrt(3. / 2), np.sqrt(2. / 5),
+        #                  1. / np.sqrt(2), np.sqrt(3. / 14)])
+        # d0_1 = np.array([0.0, 0.0,  -17. / (3 * np.sqrt(10)),
+        #                  -17. / (6 * np.sqrt(2)), -7. / 6])
+        # d1_1 = np.array([0.0, 0.0, np.sqrt(5. / 2) / 3,
+        #                  5. / (6 * np.sqrt(2)), 1. / 6])
+        # d0_2 = np.array([0.0, 0.0, 0.0, 0.0, 131. / (15 * np.sqrt(4))])
+        # d1_2 = np.array([0.0, 0.0, 0.0, 0.0, -7 * np.sqrt(7. / 2) / 12])
+        # d2_2 = np.array([0.0, 0.0, 0.0, 0.0, np.sqrt(7. / 2) / 20])
+
+        coeff_d = np.array([
+            [1.0, np.sqrt(3. / 2), np.sqrt(2. / 5), 1. / np.sqrt(2),
+             np.sqrt(3. / 14)],
+            [0.0, 0.0,  -17. / (3 * np.sqrt(10)), -17. / (6 * np.sqrt(2)),
+             -7. / 6],
+            [0.0, 0.0, np.sqrt(5. / 2) / 3, 5. / (6 * np.sqrt(2)), 1. / 6],
+            [0.0, 0.0, 0.0, 0.0, 131. / (15 * np.sqrt(4))],
+            [0.0, 0.0, 0.0, 0.0, -7 * np.sqrt(7. / 2) / 12],
+            [0.0, 0.0, 0.0, 0.0, np.sqrt(7. / 2) / 20]])
+
+        # c0_1 = np.array([0.0, np.sqrt(2), -5. / 3,
+        #                  -1 * np.sqrt(10. / 7), -np.sqrt(2)])
+        # c1_1 = np.array([0.0,  -3 / np.sqrt(2), 2. / 3,
+        #                  np.sqrt(5. / 14), 3. / (7 * np.sqrt(2))])
+        # c0_2 = np.array([0.0, 0.0, 0.0,
+        #                  3 * np.sqrt(2. / 7), 9 * np.sqrt(3. / 2) / 5])
+        # c1_2 = np.array([0.0, 0.0, 0.0,
+        #                  -155. / (12 * np.sqrt(14)), -101. / (28 * np.sqrt(6))])
+        # c2_2 = np.array([0.0, 0.0, 0.0,
+        #                  15 * np.sqrt(7. / 2) / 12, 7. / (20 * np.sqrt(6))])
+
+        coeff_c = np.array([
+            [0.0, np.sqrt(2), -5. / 3, -1 * np.sqrt(10. / 7), -np.sqrt(2)],
+            [0.0,  -3 / np.sqrt(2), 2. / 3, np.sqrt(5. / 14),
+             3. / (7 * np.sqrt(2))],
+            [0.0, 0.0, 0.0, 3 * np.sqrt(2. / 7), 9 * np.sqrt(3. / 2) / 5],
+            [0.0, 0.0, 0.0, -155. / (12 * np.sqrt(14)),
+             -101. / (28 * np.sqrt(6))],
+            [0.0, 0.0, 0.0, 5 * np.sqrt(7. / 2) / 12, 7. / (20 * np.sqrt(6))]])
+
+        num_elements = self.num_unique_elements
+        if num_elements > 6:
             raise ValueError("only compounds consisting of 2 to 6 types of"
                              " elements are supported")
 
@@ -145,3 +153,32 @@ class VandeWalle(BasisFunction):
         for x in range(self.num_unique_elements):
             spin_dict[self.unique_elements[x]] = spin_values[x]
         return spin_dict
+
+    def get_basis_function(self):
+        if alpha == 0:
+            return 1
+
+
+
+
+
+# def kronecker(i, j):
+#     if i == j:
+#         return 1
+#     return 0
+#
+# spin_dict = {"Cu":0, "Au":1, "Zn":2}
+#
+# bf1 = kronecker(sigma, 0)
+# bf2 = kronecker(sigma, 1)
+# bf3 = kronecker(sigma, 2)
+#
+# def get_basis_function():
+#     basis_function = []
+#     basis_function.append(bf1)
+#
+#     for i in range(num_elemtns):
+#         for key, value in spin_dict:
+#
+#
+#     [{"Cu":1, "Au":0, "Zn":0}, {"Cu":0, "Au":1, "Zn":0}, {"Cu":0, "Au":0, "Zn":1}]
