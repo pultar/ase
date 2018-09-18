91ad2c4e04db4171cdba46bf42cacb5f82ac0811
SJH <schinavro@SJHui-MacBook-Pro.local>
2018-09-18 14:08:23 +0900

    new branch

--- ase/test/openmx/al/al.py
+++ ase/test/openmx/al/al.py
@@ -4,17 +4,9 @@ from ase.build import bulk
 from ase.calculators.calculator import get_calculator
 
 
-required = {'abinit': dict(ecut=200, toldfe=0.0001, chksymbreak=0),
-            'aims': dict(sc_accuracy_rho=5.e-3),
-            'elk': dict(tasks=0, rgkmax=5.0),
-            'gpaw': dict(mode='pw'),
-            'cp2k': dict(auto_write=True, uks=True, max_scf=1, cutoff=400)}
-
-
 def run(name):
     Calculator = get_calculator(name)
-    par = required.get(name, {})
-    calc = Calculator(label=name, xc='LDA', kpts=(1, 1, 1), **par)
+    calc = Calculator(label=name, xc='LDA', kpts=(1, 1, 1))
     al = bulk('AlO', crystalstructure='rocksalt', a=4.5)
     al.calc = calc
     e = al.get_potential_energy()
@@ -27,7 +19,7 @@ def run(name):
     al = calc.get_atoms()
     print(al.get_potential_energy())
     label = 'dir/' + name + '-2'
-    calc = Calculator(label=label, atoms=al, xc='LDA', kpts=1.0, **par)
+    calc = Calculator(label=label, atoms=al, xc='LDA', kpts=1.0)
     print(al.get_potential_energy())
     print(Calculator.read_atoms(label).get_potential_energy())
 
