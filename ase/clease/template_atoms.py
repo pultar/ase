583369951a91e9f495beca805ecbf09092b3da89
Jin Chang <jchang@dtu.dk>
2018-10-11 10:06:28 +0200

    implemented skew threshold, select random templates with bias.

--- ase/clease/template_atoms.py
+++ ase/clease/template_atoms.py
@@ -5,7 +5,8 @@ from numpy.linalg import inv, det
 from random import choice
 
 class TemplateAtoms(object):
-    def __init__(self, supercell_factor=None, size=None, unit_cells=None):
+    def __init__(self, supercell_factor=None, size=None, unit_cells=None,
+                 skew_threshold=4):
         if size is None and supercell_factor is None:
             raise TypeError("Either size or supercell_factor needs to be "
                             "specified.\n size: list or numpy array.\n "
@@ -15,6 +16,7 @@ class TemplateAtoms(object):
         if size is None:
             self.supercell_factor = int(supercell_factor)
         self.unit_cells = unit_cells
+        self.skew_threshold = skew_threshold
         templates = self._generate_template_atoms()
         self.templates = self._filter_equivalent_templates(templates)
 
@@ -53,7 +55,8 @@ class TemplateAtoms(object):
         return abs(abs(determinant) - 1.0) < 1E-4
 
     def _filter_equivalent_templates(self, templates):
-        """Remove the symmetrically equivalent clusters."""
+        """Remove symmetrically equivalent clusters."""
+        templates = self._filter_very_skewed_templates(templates)
         filtered = {"atoms": [], "dim": []}
         for i, atom in enumerate(templates["atoms"]):
             current = atom.get_cell().T
@@ -69,8 +72,19 @@ class TemplateAtoms(object):
                 filtered["dim"].append(templates["dim"][i])
         return filtered
 
-    def random_template(self, max_supercell_factor=1000):
-        """Return a random template atoms
+    def _filter_very_skewed_templates(self, templates):
+        """Remove templates that have a very skewed unit cell
+        """
+        filtered = {"atoms": [], "dim": []}
+        for i, atoms in enumerate(templates["atoms"]):
+            ratio = self._get_max_min_diag_ratio(atoms)
+            if ratio < self.skew_threshold:
+                filtered["atoms"].append(atoms)
+                filtered["dim"].append(templates["dim"][i])
+        return filtered
+
+    def random_template(self, max_supercell_factor=1000, return_dim=False):
+        """Select a random template atoms.
 
         Arguments:
         =========
@@ -84,4 +98,40 @@ class TemplateAtoms(object):
             factor = np.prod(self.templates["dim"][num])
             if factor <= max_supercell_factor:
                 found = True
+        if return_dim:
+            return self.templates["atoms"][num], self.templates["dim"][num]
+        return self.templates["atoms"][num]
+
+    def _get_max_min_diag_ratio(self, atoms):
+        """Return the ratio between the maximum and the minimum diagonal."""
+        diag_lengths = []
+        cell = atoms.get_cell().T
+        for w in product([-1, 0, 1], repeat=3):
+            if np.allclose(w, 0):
+                continue
+            diag = cell.dot(w)
+            length = np.sqrt(diag.dot(diag))
+            diag_lengths.append(length)
+        max_length = np.max(diag_lengths)
+        min_length = np.min(diag_lengths)
+        return max_length/min_length
+
+    def weighted_random_template(self, return_dim=False):
+        """Select a random template atoms with a bias towards cells that are
+        small and has similar values for x-, y- and z-dimension sizes.
+        """
+        p_select = []
+        for dim in self.templates["dim"]:
+            mean = np.mean(dim)
+            prod = np.prod(dim)
+            p = np.exp(-np.sum(((np.array(dim)-mean)/mean)**2))
+            p_select.append(p/prod)
+        p_select = np.array(p_select)
+        p_select /= np.sum(p_select)
+
+        cum_prob = np.cumsum(p_select)
+        rand_num = np.random.rand()
+        indx = np.argmax(cum_prob > rand_num)
+        if return_dim:
+            return self.templates["atoms"][num], self.templates["dim"][num]
         return self.templates["atoms"][num]
