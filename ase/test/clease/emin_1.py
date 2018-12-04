c3592380c4cf600eae01ff9c329dd073e22ddb9f
Jin Chang <jchang@dtu.dk>
2018-12-04 10:54:44 +0100

    simple emin_1

--- ase/test/clease/emin_1.py
+++ ase/test/clease/emin_1.py
@@ -1,5 +1,5 @@
 from ase.clease import CEBulk, CorrFunction, Concentration, NewStructures, Evaluate
-from ase.clease.tools import wrap_and_sort_by_position
+from ase.clease.tools import wrap_and_sort_by_position, reconfigure
 from ase.calculators.vasp import Vasp2
 from ase.db import connect
 from ase.visualize import view
@@ -25,88 +25,9 @@ setting = CEBulk(crystalstructure='rocksalt',
                  max_cluster_dia=[7.0, 7.0, 4.0],
                  cubic=False,
                  basis_function='sanchez')
-# setting.reconfigure_settings()
-# setting.view_clusters()
-# setting._set_active_template_by_uid(4)
-# print(setting.cluster_info_by_name('c4_02nn_1'))
+
+# reconfigure(setting)
 # exit()
-print(setting.cluster_info_by_name('c3_03nn_0')[0]['indices'])
-# print(len(setting.cluster_info_by_name('c4_02nn_1')[0]['indices']))
-# print(setting.cluster_info_by_name('c4_02nn_1')[0]['max_cluster_dia'])
-# print(setting.cluster_info_by_name('c3_06nn_2')[0]['descriptor'])
-# print(setting.cluster_info_by_name('c3_06nn_3')[0]['descriptor'])
-# print(len(setting.cluster_names))
-# cf = CorrFunction(setting)
-# cf.reconfig_db_entries()
-
-# exit()
-
-# for i in range(setting.template_atoms.num_templates):
-#     setting._set_active_template_by_uid(i)
-#     print(i, setting.template_atoms.get_size()[i])
-
-#     cluster_names_setting = sorted(setting.cluster_names)
-
-#     db = connect(db_name)
-
-#     kvp = connect(db_name).get(15).key_value_pairs
-#     cluster_names_db = []
-#     for key in kvp.keys():
-#         if key.startswith(('c0', 'c1', 'c2', 'c3', 'c4', 'c5',
-#                            'c6', 'c7', 'c8', 'c9')):
-#             cluster_names_db.append(key)
-#     cluster_names_db = sorted(cluster_names_db)
-
-#     # print(len(cluster_names_db))
-#     print(len(cluster_names_setting))
-#     assert len(cluster_names_setting) == len(cluster_names_db)
-#     for i in range(len(cluster_names_setting)):
-#         assert cluster_names_setting[i] == cluster_names_db[i]
-
-# db = connect(db_name)
-# ids = [row.id for row in db.select(struct_type='initial')]
-
-# cluster_names_matrix = []
-# for id in ids:
-#     kvp = connect(db_name).get(id).key_value_pairs
-#     cluster_names_db = []
-#     for key in kvp.keys():
-#         if key.startswith(('c0', 'c1', 'c2', 'c3', 'c4', 'c5',
-#                            'c6', 'c7', 'c8', 'c9')):
-#             cluster_names_db.append(key)
-#     cluster_names_db = sorted(cluster_names_db)
-#     cluster_names_matrix.append(cluster_names_db)
-
-
-
-
-
-# ids = [row.id for row in db.select(struct_type='initial')]
-# for id in ids:
-#     kvp = connect(db_name).get(id).key_value_pairs
-#     cluster_names_db = []
-#     for key in kvp.keys():
-#         if key.startswith(('c0', 'c1', 'c2', 'c3', 'c4', 'c5',
-#                            'c6', 'c7', 'c8', 'c9')):
-#             cluster_names_db.append(key)
-#     cluster_names_db = sorted(cluster_names_db)
-
-#     assert len(cluster_names_setting) == len(cluster_names_db)
-#     for i in range(len(cluster_names_setting)):
-#         assert cluster_names_setting[i] == cluster_names_db[i]
-#         print('c3_06nn_3_200' in cluster_names_setting, 'c3_06nn_3_200' in cluster_names_db)
-
-# double = 7.0
-# triple = 3.0
-
-# eval = Evaluate(setting=setting, parallel=True, num_core=10,
-#                 fitting_scheme='l1', max_cluster_size=4,
-#                 max_cluster_dia=[double, triple, 0.0])
-# alpha = eval.plot_CV(alpha_min=1E-7, alpha_max=1.0, num_alpha=200,
-#                      scale='log')
-# eval.set_fitting_scheme(fitting_scheme='l1', alpha=alpha)
-# # eval.plot_fit(interactive=True)
-# eval.save_cluster_name_eci()
 
 # ns = NewStructures(setting=setting, generation_number=1, struct_per_gen=5)
 # ns.generate_probe_structure(approx_mean_var=True)
@@ -119,51 +40,11 @@ clusters = [x.strip() for x in clusters]
 eval = Evaluate(setting=setting, cluster_names=clusters, fitting_scheme='l2',
                 alpha=1E-7)
 cluster_name_eci = eval.get_cluster_name_eci()
-# print(cluster_name_eci)
 
-atoms = connect(db_name).get(53).toatoms()
-from ase.calculators.clease import Clease
-calc = Clease(setting, cluster_name_eci=cluster_name_eci)
-corr_func = CorrFunction(setting)
-#atoms.set_calculator(calc)
+atoms = connect('emin_1.db').get(53).toatoms()
 
-from ase.visualize import view
-#view(atoms)
-print(setting.basis_functions)
-atoms = setting.atoms.copy()
-atoms.set_calculator(calc)
-# view(atoms)
-# exit()
-atoms[1].symbol = 'Mn'
-#atoms[9].symbol = 'X'
-
-
-# atoms[12].symbol = "Li"
-# atoms[2].symbol = "Mn"
-# view(setting.atoms)
-init_cf = calc.get_cf_dict()
-energy = atoms.get_potential_energy()
-cf_brute = corr_func.get_cf_by_cluster_names(atoms, cluster_name_eci)
-cf_calc = calc.get_cf_dict()
-for k in cf_brute.keys():
-    indx = calc.cluster_names.index(k)
-    calc_change = cf_calc[k] - init_cf[k]
-    brute_change = cf_brute[k] - init_cf[k]
-    print(k, init_cf[k], brute_change, calc_change, calc_change/brute_change)
-
-# ns = NewStructures(setting=setting, generation_number=1, struct_per_gen=5)
-# ns.generate_Emin_structure(atoms=atoms, init_temp=10000, final_temp=1,
-#                            num_temp=3, num_steps_per_temp=100,
-#                            cluster_name_eci=cluster_name_eci,
-#                            random_composition=True)
-# 3
-# 24
-# 12
-# 36
-#
-# [0 1 1]
-#
-# (x0*x1*x1 + x1*x0*x0)
-#
-# [0 1 2]
-# (x0*x1*x2 + x1*x0*x2 + x2*x1*x0)
+ns = NewStructures(setting=setting, generation_number=1, struct_per_gen=2)
+ns.generate_Emin_structure(atoms=atoms, init_temp=10000, final_temp=1,
+                           num_temp=3, num_steps_per_temp=100,
+                           cluster_name_eci=cluster_name_eci,
+                           random_composition=True)
