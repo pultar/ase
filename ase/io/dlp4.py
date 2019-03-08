46a618a1487fdaaef701ad90e4a48994d205ee62
Patrick Melix <patrick.melix@uni-leipzig.de>
2019-03-08 11:01:24 +0100

    small fix for iread_dlp_history and test for history reader

--- ase/io/dlp4.py
+++ ase/io/dlp4.py
@@ -71,6 +71,7 @@ def iread_dlp_history(f, symbols=None):
     """Generator version of read_history"""
     levcfg,imcon,natoms,pos = _get_frame_positions(f)
     for p in pos:
+        f.seek(p+1)
         yield read_single_image(f, levcfg, imcon, natoms, is_trajectory=True, symbols=symbols)
 
 
