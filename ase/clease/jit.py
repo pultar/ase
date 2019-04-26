211404296317687713d3cbdc5e85655dbbd49d4f
David Kleiven <davidkleiven446@gmail.com>
2019-04-26 13:38:14 +0200

    Print message to know that Numba is used.

--- ase/clease/jit.py
+++ ase/clease/jit.py
@@ -1,6 +1,7 @@
 try:
     from numba import jit as numba_jit
     jit = numba_jit
+    print("Using JIT from Numba")
 except ImportError:
     # Numba is not installed
     def dummy_jit(**options):
