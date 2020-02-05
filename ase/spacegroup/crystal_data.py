c3c491ea99fd267ce55711a0618471c5021cbb29
pmla <peter.mahler.larsen@gmail.com>
2020-02-05 11:28:47 +0100

    added point group information

--- ase/spacegroup/crystal_data.py
+++ ase/spacegroup/crystal_data.py
@@ -12,9 +12,49 @@ _lattice_centering = ('ØPPPPCPPCCPPCPPCPPPPCCFIIPPPPPPPPPPCCCCCCCFFIIIPPPPPPPP'
                       'PPPRRPPPPPPPPPPPPPPPPPPPPPPPPPPPPFIPIPPFFIPIPPFFIPPIPF'
                       'IPFIPPPPFFFFII')
 
+_point_group_ranges = [(1, '1'),
+                       (2, '-1'),
+                       (3, '2'),
+                       (6, 'm'),
+                       (10, '2/m'),
+                       (16, '222'),
+                       (25, 'mm2'),
+                       (47, '2/m 2/m 2/m'),
+                       (75, '4'),
+                       (81, '-4'),
+                       (83, '4/m'),
+                       (89, '422'),
+                       (99, '4mm'),
+                       (111, '-42m'),
+                       (123, '4/m 2/m 2/m'),
+                       (143, '3'),
+                       (147, '-3'),
+                       (149, '32'),
+                       (156, '3m'),
+                       (162, '-3 2/m'),
+                       (168, '6'),
+                       (174, '-6'),
+                       (175, '6/m'),
+                       (177, '622'),
+                       (183, '6mm'),
+                       (187, '-6m2'),
+                       (191, '6/m 2/m 2/m'),
+                       (195, '23'),
+                       (200, '2/m -3'),
+                       (207, '432'),
+                       (215, '-43m'),
+                       (221, '4/m -3 2/m'),
+                       (231, 'Ø')]
+
+_point_groups = ['Ø']
+for i, (start, pg) in enumerate(_point_group_ranges[:-1]):
+    next_start, _ = _point_group_ranges[i + 1]
+    count = next_start - start
+    for j in range(start, start + count):
+        _point_groups.append(pg)
+
 
 def get_bravais_class(sg):
-
     sg = int(sg)
     if sg < 1:
         raise ValueError('Spacegroup must be positive, but is {}'.format(sg))
@@ -22,3 +62,12 @@ def get_bravais_class(sg):
         raise ValueError('Bad spacegroup', sg)
     pearson_symbol = _lattice_system[sg] + _lattice_centering[sg]
     return bravais_classes[pearson_symbol]
+
+
+def get_point_group(sg):
+    sg = int(sg)
+    if sg < 1:
+        raise ValueError('Spacegroup must be positive, but is {}'.format(sg))
+    if sg > 230:
+        raise ValueError('Bad spacegroup', sg)
+    return _point_groups[sg]
