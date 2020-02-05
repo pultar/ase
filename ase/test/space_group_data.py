a53f3c251861271ff64abbc55aeee337127f5ac5
pmla <peter.mahler.larsen@gmail.com>
2020-02-05 18:32:55 +0100

    better testing

--- ase/test/space_group_data.py
+++ ase/test/space_group_data.py
@@ -2,14 +2,17 @@ import pytest
 from ase.spacegroup import (get_bravais_class,
                             get_point_group,
                             polar_space_group)
+import ase.lattice
 
 
 functions = [get_bravais_class, get_point_group, polar_space_group]
 
 
-@pytest.mark.parametrize("func", functions)
-def test_valid_spacegroup(func):
-    func(1)
+@pytest.mark.parametrize("func,answer",
+                         zip(functions,
+                             [ase.lattice.FCC, '4/m -3 2/m', False]))
+def test_valid_spacegroup(func, answer):
+    assert func(225) == answer
 
 
 @pytest.mark.parametrize("func", functions)
