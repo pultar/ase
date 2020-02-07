783bf3cff7081523cfc432dd9a966cf90ae5691a
SJH <schinavro@SJHui-MacBook-Pro.local>
2020-02-07 12:01:59 +0900

    FIX: Standard unit add fix

--- ase/calculators/openmx/writer.py
+++ ase/calculators/openmx/writer.py
@@ -143,7 +143,7 @@ def parameters_to_keywords(label=None, atoms=None, parameters=None,
         parameter = parameters.get(ase_parameter)
         if parameter is not None:
             # Handles the unit
-            unit = unit_dict.get(openmx_keyword)
+            unit = standard_units[unit_dict.get(openmx_keyword)]
             if unit is not None:
                 return parameters[counterparts[openmx_keyword]] / unit
             return parameter
