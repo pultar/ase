258e653c94dae6d048a8d8fec8c9a4bc90cea55e
pmla <peter.mahler.larsen@gmail.com>
2020-02-27 10:58:45 +0100

    added test for `isolate_components`

--- ase/test/dimensionality.py
+++ ase/test/dimensionality.py
@@ -1,8 +1,9 @@
 import pytest
 import ase.build
+from ase import Atoms
 from ase.lattice.cubic import FaceCenteredCubic
-from ase.geometry.dimensionality import analyze_dimensionality
-
+from ase.geometry.dimensionality import (analyze_dimensionality,
+                                         isolate_components)
 
 @pytest.mark.parametrize("method", ['TSA', 'RDA'])
 def test_mx2(method):
@@ -32,3 +33,27 @@ def test_fcc(method):
     intervals = analyze_dimensionality(atoms, method=method)
     m = intervals[0]
     assert m.dimtype == '3D'
+
+
+def test_isolation_1D():
+    atoms = Atoms(symbols='Cl6Ti2', pbc=True,
+                  cell=[[6.27, 0, 0],
+                        [-3.135, 5.43, 0],
+                        [0, 0, 5.82]],
+                  positions=[[1.97505, 0, 1.455],
+                             [0.987525, 1.71044347, 4.365],
+                             [-0.987525, 1.71044347, 1.455],
+                             [4.29495, 0, 4.365],
+                             [2.147475, 3.71953581, 1.455],
+                             [-2.147475, 3.71953581, 4.365],
+                             [0, 0, 0],
+                             [0, 0, 2.91]])
+
+    result = isolate_components(atoms)
+    assert len(result) == 1
+    key, components = list(result.items())[0]
+    assert key == '1D'
+    assert len(components) == 1
+    chain = components[0]
+    assert (chain.pbc == [False, False, True]).all()
+    assert chain.get_chemical_formula() == atoms.get_chemical_formula()
