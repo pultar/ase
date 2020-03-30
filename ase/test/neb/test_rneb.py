4972ae65caf98a63262960f7c01b0826b4d93523
Steen Lysgaard <stly@Steens-MacBook-Pro.local>
2020-03-30 16:30:29 +0200

    initial rneb test

---
+++ ase/test/neb/test_rneb.py
@@ -0,0 +1,47 @@
+def test_rneb():
+    from ase.build import fcc100, add_adsorbate
+    from ase.constraints import FixAtoms
+    from ase.calculators.emt import EMT
+    from ase.optimize import QuasiNewton
+    from ase.neb import NEB
+    from ase.optimize import BFGS
+
+    # 2x2-Al(001) surface with 3 layers and an
+    # Au atom adsorbed in a hollow site:
+    slab = fcc100('Al', size=(2, 2, 3))
+    add_adsorbate(slab, 'Au', 1.7, 'hollow')
+    slab.center(axis=2, vacuum=4.0)
+
+    # Fix second and third layers:
+    mask = [atom.tag > 1 for atom in slab]
+    constraint = FixAtoms(mask=mask)
+    slab.set_constraint(constraint)
+
+    # Use EMT potential:
+    slab.set_calculator(EMT())
+
+    # Initial state:
+    qn = QuasiNewton(slab, trajectory='initial.traj')
+    qn.run(fmax=0.05)
+    initial = slab.copy()
+
+    # Final state:
+    slab[-1].x += slab.get_cell()[0, 0] / 2
+    qn = QuasiNewton(slab, trajectory='final.traj')
+    qn.run(fmax=0.05)
+
+    final = slab.copy()
+
+    images = [initial]
+    for i in range(3):
+        image = initial.copy()
+        image.set_calculator(EMT())
+        image.set_constraint(constraint)
+        images.append(image)
+
+    images.append(final)
+
+    neb = NEB(images)
+    neb.interpolate()
+    qn = BFGS(neb, trajectory='neb.traj')
+    qn.run(fmax=0.05)
