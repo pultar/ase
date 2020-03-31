ae9de97183fd0a0237b9ddf592917662808ac73e
Steen Lysgaard <stly@Steens-MacBook-Pro.local>
2020-03-31 16:44:05 +0200

    test rneb on slab

--- ase/test/neb/test_rneb.py
+++ ase/test/neb/test_rneb.py
@@ -1,15 +1,18 @@
-def test_rneb():
+def test_slab_rneb():
     from ase.build import fcc100, add_adsorbate
     from ase.constraints import FixAtoms
     from ase.calculators.emt import EMT
-    from ase.optimize import QuasiNewton
     from ase.neb import NEB
     from ase.optimize import BFGS
+    import numpy as np
 
-    # 2x2-Al(001) surface with 3 layers and an
+    from ase.rneb import RNEB
+
+    rneb = RNEB(logfile=None)
+
+    # 3x3-Al(001) surface with 3 layers and an
     # Au atom adsorbed in a hollow site:
-    slab = fcc100('Al', size=(2, 2, 3))
-    add_adsorbate(slab, 'Au', 1.7, 'hollow')
+    slab = fcc100('Al', size=(3, 3, 3))
     slab.center(axis=2, vacuum=4.0)
 
     # Fix second and third layers:
@@ -17,31 +20,60 @@ def test_rneb():
     constraint = FixAtoms(mask=mask)
     slab.set_constraint(constraint)
 
+    add_adsorbate(slab, 'Au', 1.7, 'hollow')
+    add_adsorbate(slab, 'Au', 1.7, 'hollow', offset=(1, 0))
+    initial_unrelaxed = slab.copy()
+    initial_unrelaxed.pop(-1)
+
     # Use EMT potential:
     slab.set_calculator(EMT())
 
     # Initial state:
-    qn = QuasiNewton(slab, trajectory='initial.traj')
+    initial_relaxed = initial_unrelaxed.copy()
+    initial_relaxed.set_calculator(EMT())
+    qn = BFGS(initial_relaxed, logfile=None)
     qn.run(fmax=0.05)
-    initial = slab.copy()
 
     # Final state:
-    slab[-1].x += slab.get_cell()[0, 0] / 2
-    qn = QuasiNewton(slab, trajectory='final.traj')
+    final_unrelaxed = slab.copy()
+    final_unrelaxed.pop(-2)
+
+    # RNEB symmetry identification
+    sym_ops = rneb.find_symmetries(slab, initial_unrelaxed, final_unrelaxed)
+
+    # Obtain the final relaxed structure with RNEB
+    final_relaxed = rneb.get_final_image(slab, initial_unrelaxed,
+                                         initial_relaxed, final_unrelaxed)
+
+    images = create_path(initial_relaxed, final_relaxed)
+
+    neb = NEB(images, sym=True, rotations=sym_ops[0])
+    neb.interpolate()
+    qn = BFGS(neb, logfile=None)
     qn.run(fmax=0.05)
 
-    final = slab.copy()
+    sym_image_energy = images[-2].get_potential_energy()
 
-    images = [initial]
+    # Do a normal NEB
+    neb_images = create_path(initial_relaxed, final_relaxed)
+    normal_neb = NEB(neb_images)
+    normal_neb.interpolate()
+    qn = BFGS(normal_neb, logfile=None)
+    qn.run(fmax=0.05)
+
+    normal_image_energy = neb_images[-2].get_potential_energy()
+
+    assert np.isclose(sym_image_energy, normal_image_energy)
+
+
+def create_path(init, final):
+    from ase.calculators.emt import EMT
+
+    images = [init]
     for i in range(3):
-        image = initial.copy()
+        image = init.copy()
         image.set_calculator(EMT())
-        image.set_constraint(constraint)
         images.append(image)
 
     images.append(final)
-
-    neb = NEB(images)
-    neb.interpolate()
-    qn = BFGS(neb, trajectory='neb.traj')
-    qn.run(fmax=0.05)
+    return images
