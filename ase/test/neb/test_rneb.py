62a280785cfa9e32138d750bb153b3e0bcf520e8
FelixBoelle <felixtim.boelle@gmail.com>
2020-04-02 16:43:17 +0200

    added is_reflective check test and example

--- ase/test/neb/test_rneb.py
+++ ase/test/neb/test_rneb.py
@@ -36,10 +36,16 @@ def test_slab_rneb():
 
     # Final state:
     final_unrelaxed = slab.copy()
-    final_unrelaxed.pop(-2)
+    final_unrelaxed.pop
 
     # RNEB symmetry identification
     sym_ops = rneb.find_symmetries(slab, initial_unrelaxed, final_unrelaxed)
+    # check that path has reflection symmetry for each images
+    images = create_path(initial_unrelaxed, final_unrelaxed)
+    neb = NEB(images)
+    neb.interpolate(images)
+    sym_ops = rneb.reflect_path(images, sym=sym_ops)
+    print(sym_ops)
 
     # Obtain the final relaxed structure with RNEB
     final_relaxed = rneb.get_final_image(slab, initial_unrelaxed,
@@ -77,3 +83,148 @@ def create_path(init, final):
 
     images.append(final)
     return images
+
+def test_is_reflective():
+    """ Test to check paths separately for reflection symmetry """
+    from ase.build import fcc111
+    
+    atoms = fcc111('Cu', [2,2,1], 4, periodic=True)
+    atoms = atoms.repeat([2,2,1])
+    
+    # give path to look at as path=(init_indice, final_indice)
+    get_num_sym_operators(atoms, path=(1, 2)) # here i get 4
+    get_num_sym_operators(atoms, path=(0, 1)) # here i get only 2!?
+
+def get_num_sym_operators(atoms, path):
+    from ase.neb import NEB
+    
+    from ase.rneb import RNEB
+    # deleting ions will change inidices
+    initial_unrelaxed = atoms.copy()
+    del initial_unrelaxed[path[0]]
+    
+    final_unrelaxed = atoms.copy()
+    del final_unrelaxed[path[1]]
+    
+    # aling indices
+    final_unrelaxed = align_indices(initial_unrelaxed, final_unrelaxed)
+    images = create_path(initial_unrelaxed, final_unrelaxed)
+    neb = NEB(images)
+    neb.interpolate()
+    
+    rneb = RNEB(logfile=None)
+    sym = rneb.find_symmetries(atoms, initial_unrelaxed, final_unrelaxed)
+    sym = rneb.reflect_path(images, sym=sym)
+    print(f"Allowed reflective operations for {path[0]}->{path[1]}: {len(sym)}")
+    assert sym is not None # otherwise not reflective
+    
+def align_indices(initial, final):
+    """
+    move the atoms in final such that the indices match with initial.
+
+    Parameters
+    ---------
+    initial: ASE atoms-object
+
+    final: ASE atoms-object
+
+    Return
+    -------
+    ASE-atoms object
+    """
+
+    sort_list = match_atoms(initial, final)    
+    final_sorted = final.copy()
+    final_sorted = final_sorted[sort_list]
+
+    return final_sorted
+
+def match_atoms(initial, final):
+    """
+    Match the atomic indices in final to the ones in initial based on
+    position
+
+    Parameters
+    ---------
+
+    initial: ASE atoms-object
+    The initial unrelaxed structure
+
+    final: ASE atoms-object
+    The final unrelaxed structure
+
+    Returns
+    -------
+
+    sorting list: numpy array 1 x n of integers
+    """
+    import numpy as np
+    pos_initial = initial.get_positions()
+    pos_final = final.get_positions()
+    symb_initial = initial.get_chemical_symbols()
+    symb_final = final.get_chemical_symbols()
+    sort_list = np.ones(len(initial)).astype(int)*-1
+    no_match = []
+    for idx_final, state in enumerate(zip(pos_final, symb_final)):
+        pf, sf = state
+        idx_initial = find_atom(pos_initial, symb_initial, pf, sf,
+                                  critical=False)
+        if idx_initial is False:
+            no_match.append(idx_final)
+        else:        
+            sort_list[idx_initial] = idx_final
+    missing = np.where(sort_list == -1)  
+    if len(missing) != len(no_match):
+        if missing[0] != no_match[0]:
+            msg = ("different number of atoms have moved "
+                   "in the initial and final structures")
+            raise RuntimeError(msg)
+    
+    if len(no_match) > 1:
+        msg = ("Found more than one moving atom. "
+               "Please give information about where "
+               "the moving atoms start and finish")
+        raise RuntimeError(msg)
+    sort_list[missing[0]] = no_match[0]
+    return sort_list
+
+def find_atom(pos, symb, pos_d, symb_d, critical=True, tol=1e-3):
+    """
+    Find the atom matching pos_d.
+
+    Parameters
+    ---------
+
+    pos: numpy array n x 3
+    positions to be matched against
+
+    sym: string
+    chemical symbols to be matched against
+
+    pos_d: numpy array 1 x 3
+    position to be found
+
+    sym_d: string
+    chemical symbols to be found
+
+    critical: boolean
+    whether to fail upon not being able to make a match
+
+    Return
+    -------
+    integer if match is found otherwise False: integer or boolean
+    """
+    import numpy as np
+    for i, state in enumerate(zip(pos, symb)):
+        p, s = state
+        if s == symb_d:
+            dist = np.linalg.norm(p-pos_d)
+            if dist < tol:
+                return i
+    if critical:
+        msg = ("The speified atoms was not found. "
+               "Make sure you given the correct positions and symbols")
+        raise RuntimeError(msg)
+    else:
+        return False
+
