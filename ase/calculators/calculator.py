2c0cf192edaf8104b8bbffeac82ee94643d83837
Amish <amish.cy@gmail.com>
2020-04-23 15:53:46 -0400

    Added 'must' to names variable in ase/calculators/calculator.py

--- ase/calculators/calculator.py
+++ ase/calculators/calculator.py
@@ -124,7 +124,7 @@ names = ['abinit', 'ace', 'aims', 'amber', 'asap', 'castep', 'cp2k',
          'crystal', 'demon', 'demonnano', 'dftb', 'dftd3', 'dmol', 'eam', 'elk',
          'emt', 'espresso', 'exciting', 'ff', 'fleur', 'gamess_us', 'gaussian',
          'gpaw', 'gromacs', 'gulp', 'hotbit', 'kim',
-         'lammpslib', 'lammpsrun', 'lj', 'mopac', 'morse', 'nwchem',
+         'lammpslib', 'lammpsrun', 'lj', 'mopac', 'morse' , 'must', 'nwchem',
          'octopus', 'onetep', 'openmx', 'orca', 'psi4', 'qchem', 'siesta',
          'tip3p', 'tip4p', 'turbomole', 'vasp']
 
