1c9a31596eba6c36adffc7527e9e130954c2c2e1
pmla <peter.mahler.larsen@gmail.com>
2020-05-14 14:18:30 +0200

    added cycle checking for Minkowski reduction

--- ase/test/cell/test_minkowski_reduce.py
+++ ase/test/cell/test_minkowski_reduce.py
@@ -1,77 +1,90 @@
+import pytest
 import numpy as np
 from ase.geometry import minkowski_reduce
 from ase.cell import Cell
 from ase.utils import seterr
 
 
-def test_minkowski():
-    with seterr(all='raise'):
-        run()
+TOL = 1E-14
+seterr(all='raise')
 
 
-def run():
-    tol = 1E-14
-    rng = np.random.RandomState(0)
-
-
+def test_issue():
     cell = Cell([[8.972058879514716, 0.0009788104586639142, 0.0005932485724084841],
                  [4.485181755775297, 7.770520334862034, 0.00043663339838788054],
                  [4.484671994095723, 2.5902066679984634, 16.25695615743613]])
     cell.minkowski_reduce()
 
-    for i in range(40):
-        B = rng.uniform(-1, 1, (3, 3))
-        R, H = minkowski_reduce(B)
-        assert np.allclose(H @ B, R, atol=tol)
-        assert np.sign(np.linalg.det(B)) == np.sign(np.linalg.det(R))
 
-        norms = np.linalg.norm(R, axis=1)
-        assert (np.argsort(norms) == range(3)).all()
-
-        # Test idempotency
-        _, _H = minkowski_reduce(R)
-        assert (_H == np.eye(3).astype(np.int)).all()
-
-        rcell, _ = Cell(B).minkowski_reduce()
-        assert np.allclose(rcell, R, atol=tol)
+def test_cycle():
+    # Without cycle-checking in the MR code, this cell causes failure
+    a, b, c = 4.374006080444519, 2.0714140579127145, 3.671070851026261
+    cell = np.array([[-a, b, c], [a, c, b], [a, b, c]])
+    minkowski_reduce(cell)
 
 
-    cell = np.array([[1, 1, 2], [0, 1, 4], [0, 0, 1]])
-    unimodular = np.array([[1, 2, 2], [0, 1, 2], [0, 0, 1]])
-    assert np.linalg.det(unimodular) == 1
-    lcell = unimodular.T @ cell
+@pytest.mark.parametrize("it", range(10))
+def test_random(it):
+    rng = np.random.RandomState(seed=it)
+    B = rng.uniform(-1, 1, (3, 3))
+    R, H = minkowski_reduce(B)
+    assert np.allclose(H @ B, R, atol=TOL)
+    assert np.sign(np.linalg.det(B)) == np.sign(np.linalg.det(R))
 
-    # test 3D
-    rcell, op = minkowski_reduce(lcell)
-    assert np.linalg.det(rcell) == 1
+    norms = np.linalg.norm(R, axis=1)
+    assert (np.argsort(norms) == range(3)).all()
 
-    for pbc in [1, True, (1, 1, 1)]:
-        rcell, op = minkowski_reduce(lcell, pbc=pbc)
+    # Test idempotency
+    _, _H = minkowski_reduce(R)
+    assert (_H == np.eye(3).astype(np.int)).all()
+
+    rcell, _ = Cell(B).minkowski_reduce()
+    assert np.allclose(rcell, R, atol=TOL)
+
+
+class TestKnownUnimodularMatrix(object):
+
+    def setup_method(self):
+        cell = np.array([[1, 1, 2], [0, 1, 4], [0, 0, 1]])
+        unimodular = np.array([[1, 2, 2], [0, 1, 2], [0, 0, 1]])
+        assert np.linalg.det(unimodular) == 1
+        self.lcell = unimodular.T @ cell
+
+    def test_3d(self):
+        lcell = self.lcell
+        rcell, op = minkowski_reduce(lcell)
         assert np.linalg.det(rcell) == 1
-        assert np.sign(np.linalg.det(rcell)) == np.sign(np.linalg.det(lcell))
 
-    # test 0D
-    rcell, op = minkowski_reduce(lcell, pbc=[0, 0, 0])
-    assert (rcell == lcell).all()    # 0D reduction does nothing
+        for pbc in [1, True, (1, 1, 1)]:
+            rcell, op = minkowski_reduce(lcell, pbc=pbc)
+            assert np.linalg.det(rcell) == 1
+            assert np.sign(np.linalg.det(rcell)) == np.sign(np.linalg.det(lcell))
 
-    # test 1D
-    for i in range(3):
-        rcell, op = minkowski_reduce(lcell, pbc=np.roll([1, 0, 0], i))
+    def test_0d(self):
+        lcell = self.lcell
+        rcell, op = minkowski_reduce(lcell, pbc=[0, 0, 0])
+        assert (rcell == lcell).all()    # 0D reduction does nothing
+
+    @pytest.mark.parametrize("axis", range(3))
+    def test_1d(self, axis):
+        lcell = self.lcell
+        rcell, op = minkowski_reduce(lcell, pbc=np.roll([1, 0, 0], axis))
         assert (rcell == lcell).all()    # 1D reduction does nothing
 
-    zcell = np.zeros((3, 3))
-    zcell[0] = lcell[0]
-    rcell, _ = Cell(zcell).minkowski_reduce()
-    assert np.allclose(rcell, zcell, atol=tol)
+        zcell = np.zeros((3, 3))
+        zcell[0] = lcell[0]
+        rcell, _ = Cell(zcell).minkowski_reduce()
+        assert np.allclose(rcell, zcell, atol=TOL)
 
-    # test 2D
-    for i in range(3):
-        pbc = np.roll([0, 1, 1], i)
+    @pytest.mark.parametrize("axis", range(3))
+    def test_2d(self, axis):
+        lcell = self.lcell
+        pbc = np.roll([0, 1, 1], axis)
         rcell, op = minkowski_reduce(lcell.astype(np.float), pbc=pbc)
-        assert (rcell[i] == lcell[i]).all()
+        assert (rcell[axis] == lcell[axis]).all()
 
-        zcell = np.copy(lcell.astype(np.float))
-        zcell[i] = 0
+        zcell = np.copy(lcell)
+        zcell[axis] = 0
         rzcell, _ = Cell(zcell).minkowski_reduce()
-        rcell[i] = 0
-        assert np.allclose(rzcell, rcell, atol=tol)
+        rcell[axis] = 0
+        assert np.allclose(rzcell, rcell, atol=TOL)
