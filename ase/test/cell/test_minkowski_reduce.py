83f99ebf34da0b017af4b396779b53a710012d9f
pmla <peter.mahler.larsen@gmail.com>
2020-05-14 21:00:53 +0200

    post-review fixes

--- ase/test/cell/test_minkowski_reduce.py
+++ ase/test/cell/test_minkowski_reduce.py
@@ -1,12 +1,11 @@
 import pytest
 import numpy as np
+from numpy.testing import assert_allclose, assert_almost_equal
 from ase.geometry import minkowski_reduce
 from ase.cell import Cell
-from ase.utils import seterr
 
 
 TOL = 1E-14
-seterr(all='raise')
 
 
 def test_issue():
@@ -29,7 +28,7 @@ def test_random(it):
     rng = np.random.RandomState(seed=it)
     B = rng.uniform(-1, 1, (3, 3))
     R, H = minkowski_reduce(B)
-    assert np.allclose(H @ B, R, atol=TOL)
+    assert_allclose(H @ B, R, atol=TOL)
     assert np.sign(np.linalg.det(B)) == np.sign(np.linalg.det(R))
 
     norms = np.linalg.norm(R, axis=1)
@@ -40,22 +39,22 @@ def test_random(it):
     assert (_H == np.eye(3).astype(np.int)).all()
 
     rcell, _ = Cell(B).minkowski_reduce()
-    assert np.allclose(rcell, R, atol=TOL)
+    assert_allclose(rcell, R, atol=TOL)
 
 
-class TestKnownUnimodularMatrix(object):
+class TestKnownUnimodularMatrix():
 
     def setup_method(self):
         cell = np.array([[1, 1, 2], [0, 1, 4], [0, 0, 1]])
         unimodular = np.array([[1, 2, 2], [0, 1, 2], [0, 0, 1]])
-        assert np.linalg.det(unimodular) == 1
+        assert_almost_equal(np.linalg.det(unimodular), 1)
         self.lcell = unimodular.T @ cell
 
     @pytest.mark.parametrize("pbc", [1, True, (1, 1, 1)])
     def test_pbc(self, pbc):
         lcell = self.lcell
         rcell, op = minkowski_reduce(lcell, pbc=pbc)
-        assert np.linalg.det(rcell) == 1
+        assert_almost_equal(np.linalg.det(rcell), 1)
 
         rdet = np.linalg.det(rcell)
         ldet = np.linalg.det(lcell)
@@ -75,7 +74,7 @@ class TestKnownUnimodularMatrix(object):
         zcell = np.zeros((3, 3))
         zcell[0] = lcell[0]
         rcell, _ = Cell(zcell).minkowski_reduce()
-        assert np.allclose(rcell, zcell, atol=TOL)
+        assert_allclose(rcell, zcell, atol=TOL)
 
     @pytest.mark.parametrize("axis", range(3))
     def test_2d(self, axis):
@@ -88,9 +87,9 @@ class TestKnownUnimodularMatrix(object):
         zcell[axis] = 0
         rzcell, _ = Cell(zcell).minkowski_reduce()
         rcell[axis] = 0
-        assert np.allclose(rzcell, rcell, atol=TOL)
+        assert_allclose(rzcell, rcell, atol=TOL)
 
     def test_3d(self):
         lcell = self.lcell
         rcell, op = minkowski_reduce(lcell)
-        assert np.linalg.det(rcell) == 1
+        assert_almost_equal(np.linalg.det(rcell), 1)
