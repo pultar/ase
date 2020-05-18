8b54392a2c96118953e9c6e89afbb3eeec9028fc
Adam J. Jackson <a.j.jackson@physics.org>
2020-05-18 14:32:50 +0100

    TST: VASP kpoints read test: refactor with pytest fixtures
    Collecting tests into namedtuples with a "name" field makes it easier
    to interpret the pytest output when they fail

--- ase/test/vasp/test_read_kpoints.py
+++ ase/test/vasp/test_read_kpoints.py
@@ -1,172 +1,116 @@
 """
 Check the many ways of reading KPOINTS
 """
+from collections import namedtuple
 import os
-import pytest
+import tempfile
+
 from numpy.testing import assert_array_almost_equal
+import pytest
+
 from ase.calculators.vasp.create_input import GenerateVaspInput
 
-mp_grid_txt = """Any Comment On This Line
-              0
-              Monkhorst-Pack
-              4 4 4
-              0 0 0
-              """
+KpointTestData = namedtuple('KpointTestData',
+                            ['name', 'text', 'gamma', 'reciprocal', 'kpts'])
 
-mp_gamma_txt = """Also ANY Comment On This Line
-               0
-               g
-               2 2 2
-               0 0 0
-               """
+all_tests = [KpointTestData("MP grid",
+                            """Any Comment On This Line
+                            0
+                            Monkhorst-Pack
+                            4 4 4
+                            0 0 0
+                            """, False, None, [4, 4, 4]),
+             KpointTestData("Gamma-centered grid",
+                            """Also ANY Comment On This Line
+                            0
+                            g
+                            2 2 2
+                            0 0 0
+                            """, True, None, [2, 2, 2]),
+             KpointTestData("Auto",
+                            """Any Comment On This Line
+                            0
+                            Auto
+                            20
+                            """, False, None, [20]),
+             KpointTestData("Reciprocal points",
+                            """Any Comment On This Line
+                            5
+                            Reciprocal
+                            0.  0. 0.
+                            0.1 0. 0.
+                            0.2 0. 0.
+                            0.5 0. 0.5
+                            0.  0. 0.5
+                            """, None, True, [[0., 0., 0.],
+                                              [0.1, 0., 0.],
+                                              [0.2, 0., 0.],
+                                              [0.5, 0., 0.5],
+                                              [0., 0., 0.5]]),
+             KpointTestData("Cartesian points",
+                            """Any Comment On This Line
+                            4
+                            k
+                            0.1 0. 0.
+                            0.2 0. 0.
+                            0.5 0. 0.5
+                            0.  0. 0.5
 
-mp_auto_txt = """Any Comment On This Line
-              0
-              Auto
-              20
-              """
+                            """, None, False, [[0.1, 0., 0.],
+                                               [0.2, 0., 0.],
+                                               [0.5, 0., 0.5],
+                                               [0., 0., 0.5]]),
+             KpointTestData("Cartesian lines",
+                            """Any Comment On This Line
+                            3
+                            line
+                            CART
+                            0. 0. 0.
+                            1. 0. 0.
+                            1. 0. 0.
+                            1.  0. 1.
+                            0. 1. 0.
+                            0. 0. 0.
 
-list_recip_txt = """Any Comment On This Line
-               5
-               Reciprocal
-               0.  0. 0.
-               0.1 0. 0.
-               0.2 0. 0.
-               0.5 0. 0.5
-               0.  0. 0.5
-               """
+                            """, None, False,
+                            [[0., 0., 0.], [0.5, 0., 0.], [1., 0., 0.],
+                             [1., 0., 0.], [1., 0., 0.5], [1., 0., 1.],
+                             [0., 1., 0.], [0., 0.5, 0.], [0., 0., 0.]]),
+             KpointTestData("Reciprocal lines",
+                            """Any Comment On This Line
+                            3
+                            line
+                            rec
+                            0. 0. 0.
+                            1. 0. 0.
+                            1. 0. 0.
+                            1.  0. 1.
+                            0. 1. 0.
+                            0. 0. 0.
 
-list_cart_txt = """Any Comment On This Line
-              4
-              k
-              0.1 0. 0.
-              0.2 0. 0.
-              0.5 0. 0.5
-              0.  0. 0.5
-
-              """
-
-line_cart_txt = """Any Comment On This Line
-                3
-                line
-                CART
-                0. 0. 0.
-                1. 0. 0.
-                1. 0. 0.
-                1.  0. 1.
-                0. 1. 0.
-                0. 0. 0.
-
-                """
-
-line_recip_txt = """Any Comment On This Line
-                3
-                line
-                rec
-                0. 0. 0.
-                1. 0. 0.
-                1. 0. 0.
-                1.  0. 1.
-                0. 1. 0.
-                0. 0. 0.
-
-                """
-
-interp_points = [[0., 0., 0.], [0.5, 0., 0.], [1., 0., 0.],
-                 [1., 0., 0.], [1., 0., 0.5], [1., 0., 1.],
-                 [0., 1., 0.], [0., 0.5, 0.], [0., 0., 0.]]
+                            """, None, True,
+                            [[0., 0., 0.], [0.5, 0., 0.], [1., 0., 0.],
+                             [1., 0., 0.], [1., 0., 0.5], [1., 0., 1.],
+                             [0., 1., 0.], [0., 0.5, 0.], [0., 0., 0.]])
+             ]
 
 
-class TestReadKpoints:
-    @classmethod
-    def setup_class(self):
-        self.outfile = 'KPOINTS'
+@pytest.mark.parametrize('test_data', all_tests)
+def test_read_kpoints(test_data):
+    with tempfile.NamedTemporaryFile(mode='wt', delete=False) as fd:
+        fd.write(test_data.text)
+        tmpfilename = fd.name
 
-    @classmethod
-    def teardown_class(self):
-        if os.path.isfile(self.outfile):
-            os.remove(self.outfile)
-
-    def test_read_mp_grid(self):
-        with open(self.outfile, 'w') as fd:
-            fd.write(mp_grid_txt)
-
+    try:
         vaspinput = GenerateVaspInput()
-        vaspinput.read_kpoints(self.outfile)
+        vaspinput.read_kpoints(tmpfilename)
+    finally:
+        os.remove(tmpfilename)
 
-        assert_array_almost_equal(vaspinput.input_params['kpts'], [4, 4, 4])
-        assert not vaspinput.input_params['gamma']
+    if test_data.gamma is not None:
+        assert vaspinput.input_params['gamma'] == test_data.gamma
 
-    def test_read_gamma_grid(self):
-        with open(self.outfile, 'w') as fd:
-            fd.write(mp_gamma_txt)
+    if test_data.reciprocal is not None:
+        assert vaspinput.input_params['reciprocal'] == test_data.reciprocal
 
-        vaspinput = GenerateVaspInput()
-        vaspinput.read_kpoints(self.outfile)
-
-        assert_array_almost_equal(vaspinput.input_params['kpts'], [2, 2, 2])
-        assert vaspinput.input_params['gamma']
-
-    def test_read_auto_grid(self):
-        with open(self.outfile, 'w') as fd:
-            fd.write(mp_auto_txt)
-
-        vaspinput = GenerateVaspInput()
-        vaspinput.read_kpoints(self.outfile)
-
-        assert_array_almost_equal(vaspinput.input_params['kpts'], [20])
-        assert not vaspinput.input_params['gamma']
-
-    def test_read_recip_list(self):
-        with open(self.outfile, 'w') as fd:
-            fd.write(list_recip_txt)
-
-        vaspinput = GenerateVaspInput()
-        vaspinput.read_kpoints(self.outfile)
-
-        assert len(vaspinput.input_params['kpts']) == 5
-        assert_array_almost_equal(vaspinput.input_params['kpts'],
-                                  [[0., 0., 0.],
-                                   [0.1, 0., 0.],
-                                   [0.2, 0., 0.],
-                                   [0.5, 0., 0.5],
-                                   [0., 0., 0.5]])
-        assert vaspinput.input_params['reciprocal']
-
-    def test_read_cart_list(self):
-        with open(self.outfile, 'w') as fd:
-            fd.write(list_cart_txt)
-
-        vaspinput = GenerateVaspInput()
-        vaspinput.read_kpoints(self.outfile)
-
-        assert_array_almost_equal(vaspinput.input_params['kpts'],
-                                  [[0.1, 0., 0.],
-                                   [0.2, 0., 0.],
-                                   [0.5, 0., 0.5],
-                                   [0., 0., 0.5]])
-        assert not vaspinput.input_params['reciprocal']
-
-    def test_read_cart_lines(self):
-        with open(self.outfile, 'w') as fd:
-            fd.write(line_cart_txt)
-
-        vaspinput = GenerateVaspInput()
-        vaspinput.read_kpoints(self.outfile)
-
-        assert_array_almost_equal(vaspinput.input_params['kpts'],
-                                  interp_points)
-
-        assert not vaspinput.input_params['reciprocal']
-
-    def test_read_recip_lines(self):
-        with open(self.outfile, 'w') as fd:
-            fd.write(line_recip_txt)
-
-        vaspinput = GenerateVaspInput()
-        vaspinput.read_kpoints(self.outfile)
-
-        assert_array_almost_equal(vaspinput.input_params['kpts'],
-                                  interp_points)
-
-        assert vaspinput.input_params['reciprocal']
+    assert_array_almost_equal(vaspinput.input_params['kpts'], test_data.kpts)
