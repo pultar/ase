db7d4f803b9a7416b085b5dd5dc3c41678818470
Asmus Ougaard Dohn <asmus.od@gmail.com>
2020-07-21 18:16:13 +0200

    flake hack

--- doc/tutorials/qmmm/plot_fecn6.py
+++ doc/tutorials/qmmm/plot_fecn6.py
@@ -1,4 +1,6 @@
 # creates: fecn6.png
+atoms = ''  # flake urself
+write = ''  # will be overwritten
 exec(compile(open('build_fecn6.py').read(), 'build_fecn6.py', 'exec'))
 atoms.rotate(45, (0, 1, 1))
 write('fecn6.png', atoms)
