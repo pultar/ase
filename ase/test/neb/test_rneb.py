8781582d4879855a1fe4adde229d45db374df760
Steen Lysgaard <stly@dtu.dk>
2020-08-25 16:11:55 +0200

    get rid of print and align_indices function

--- ase/test/neb/test_rneb.py
+++ ase/test/neb/test_rneb.py
@@ -311,11 +311,11 @@ def test_number_reflection_operators(initial_structures_fcc111_al):
     atoms = initial_structures_fcc111_al[0]
 
     # give path to look at as path=(init_indice, final_indice)
-    print('Trying path 0 -> 1')
+    # Trying path 0 -> 1
     sym = get_num_sym_operators(atoms, path=(0, 1))
     assert len(sym) == 1
 
-    print('Trying path 1 -> 2')
+    # Trying path 1 -> 2
     sym = get_num_sym_operators(atoms, path=(1, 2))
     assert len(sym) == 2
 
@@ -339,121 +339,7 @@ def get_num_sym_operators(atoms, path):
     S = rneb.find_symmetries(atoms, initial_unrelaxed, final_unrelaxed)
 
     sym = rneb.reflect_path(images, sym=S)
-    print(
-        f"Allowed reflective operations for {path[0]}->{path[1]}: {len(sym)}")
     assert sym is not None  # otherwise not reflective
 
     return sym
 
-
-def align_indices(initial, final):
-    """
-    move the atoms in final such that the indices match with initial.
-
-    Parameters
-    ---------
-    initial: ASE atoms-object
-
-    final: ASE atoms-object
-
-    Return
-    -------
-    ASE-atoms object
-    """
-
-    sort_list = match_atoms(initial, final)
-    final_sorted = final.copy()
-    final_sorted = final_sorted[sort_list]
-
-    return final_sorted
-
-
-def match_atoms(initial, final):
-    """
-    Match the atomic indices in final to the ones in initial based on
-    position
-
-    Parameters
-    ---------
-
-    initial: ASE atoms-object
-    The initial unrelaxed structure
-
-    final: ASE atoms-object
-    The final unrelaxed structure
-
-    Returns
-    -------
-
-    sorting list: numpy array 1 x n of integers
-    """
-    import numpy as np
-    pos_initial = initial.get_positions()
-    pos_final = final.get_positions()
-    symb_initial = initial.get_chemical_symbols()
-    symb_final = final.get_chemical_symbols()
-    sort_list = np.ones(len(initial)).astype(int) * -1
-    no_match = []
-    for idx_final, state in enumerate(zip(pos_final, symb_final)):
-        pf, sf = state
-        idx_initial = find_atom(pos_initial, symb_initial, pf, sf,
-                                critical=False)
-        if idx_initial is False:
-            no_match.append(idx_final)
-        else:
-            sort_list[idx_initial] = idx_final
-    missing = np.where(sort_list == -1)
-    if len(missing) != len(no_match):
-        if missing[0] != no_match[0]:
-            msg = ("different number of atoms have moved "
-                   "in the initial and final structures")
-            raise RuntimeError(msg)
-
-    if len(no_match) > 1:
-        msg = ("Found more than one moving atom. "
-               "Please give information about where "
-               "the moving atoms start and finish")
-        raise RuntimeError(msg)
-    sort_list[missing[0]] = no_match[0]
-    return sort_list
-
-
-def find_atom(pos, symb, pos_d, symb_d, critical=True, tol=1e-3):
-    """
-    Find the atom matching pos_d.
-
-    Parameters
-    ---------
-
-    pos: numpy array n x 3
-    positions to be matched against
-
-    sym: string
-    chemical symbols to be matched against
-
-    pos_d: numpy array 1 x 3
-    position to be found
-
-    sym_d: string
-    chemical symbols to be found
-
-    critical: boolean
-    whether to fail upon not being able to make a match
-
-    Return
-    -------
-    integer if match is found otherwise False: integer or boolean
-    """
-    import numpy as np
-    for i, state in enumerate(zip(pos, symb)):
-        p, s = state
-        if s == symb_d:
-            dist = np.linalg.norm(p - pos_d)
-            if dist < tol:
-                return i
-    if critical:
-        msg = ("The speified atoms was not found. "
-               "Make sure you given the correct positions and symbols")
-        raise RuntimeError(msg)
-    else:
-        return False
