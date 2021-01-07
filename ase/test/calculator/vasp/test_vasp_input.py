6ab4e67acaf22699a91788b68bb5a2e6481061d9
Alexander Sougaard Tygesen <atygesen@hotmail.com>
2021-01-07 10:01:33 +0100

    remove unused molecule import

--- ase/test/calculator/vasp/test_vasp_input.py
+++ ase/test/calculator/vasp/test_vasp_input.py
@@ -6,7 +6,7 @@ from ase.calculators.vasp.create_input import GenerateVaspInput
 from ase.calculators.vasp.create_input import _args_without_comment
 from ase.calculators.vasp.create_input import _to_vasp_bool, _from_vasp_bool
 
-from ase.build import molecule, bulk
+from ase.build import bulk
 
 
 @pytest.fixture
