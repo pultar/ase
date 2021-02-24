69de461371b3146e666e4350c59ad4d0297e72a0
pthibaud <pascal.thibaudeau@cea.fr>
2021-02-24 12:34:14 +0100

    corrections for DFT+U read for espresso

--- ase/io/espresso.py
+++ ase/io/espresso.py
@@ -48,6 +48,8 @@ _PW_HIGHEST_OCCUPIED_LOWEST_FREE = 'highest occupied, lowest unoccupied level'
 _PW_KPTS = 'number of k points='
 _PW_BANDS = _PW_END
 _PW_BANDSTRUCTURE = 'End of band structure calculation'
+_PW_DFT_PLUS_U_ENTER = '--- enter write_ns ---'
+_PW_DFT_PLUS_U_EXIT = '--- exit write_ns ---'
 
 
 class Namelist(OrderedDict):
@@ -119,6 +121,8 @@ def read_espresso_out(fileobj, index=-1, results_required=True):
         _PW_KPTS: [],
         _PW_BANDS: [],
         _PW_BANDSTRUCTURE: [],
+        _PW_DFT_PLUS_U_ENTER: [],
+        _PW_DFT_PLUS_U_EXIT: [],
     }
 
     for idx, line in enumerate(pwo_lines):
@@ -139,6 +143,8 @@ def read_espresso_out(fileobj, index=-1, results_required=True):
     #   abnormally.
     # - 'relax' and 'vc-relax' re-prints the final configuration but
     #   only 'vc-relax' recalculates.
+    if indexes[_PW_DFT_PLUS_U_EXIT]:
+        indexes[_PW_BANDS] = indexes[_PW_DFT_PLUS_U_EXIT][2:] 
     if results_required:
         results_indexes = sorted(indexes[_PW_TOTEN] + indexes[_PW_FORCE] +
                                  indexes[_PW_STRESS] + indexes[_PW_MAGMOM] +
@@ -160,7 +166,7 @@ def read_espresso_out(fileobj, index=-1, results_required=True):
     else:
         image_indexes = all_config_indexes[index]
 
-    # Extract initialisation information each time PWSCF starts
+    # Extract initialization information each time PWSCF starts
     # to add to subsequent configurations. Use None so slices know
     # when to fill in the blanks.
     pwscf_start_info = dict((idx, None) for idx in indexes[_PW_START])
@@ -280,6 +286,17 @@ def read_espresso_out(fileobj, index=-1, results_required=True):
                 if image_index < holf_index < next_index:
                     efermi = float(pwo_lines[holf_index].split()[-2])
 
+        # DFT plus U read not yet implemented
+        for hubbard_index in indexes[_PW_DFT_PLUS_U_ENTER]:
+            if image_index < hubbard_index < next_index:
+                while True:
+                    L = pwo_lines[hubbard_index].replace('-', ' -').split()
+                    if L == 0:
+                        pass
+                    else:
+                        break        
+                    hubbard_index += 1
+    
         # K-points
         ibzkpts = None
         weights = None
@@ -350,7 +367,7 @@ def read_espresso_out(fileobj, index=-1, results_required=True):
                 if spin == 1:
                     assert len(eigenvalues[0]) == len(eigenvalues[1])
                 assert len(eigenvalues[0]) == len(ibzkpts), \
-                    (np.shape(eigenvalues), len(ibzkpts))
+                    (np.shape(eigenvalues)[1], len(ibzkpts))
 
                 kpts = []
                 for s in range(spin + 1):
