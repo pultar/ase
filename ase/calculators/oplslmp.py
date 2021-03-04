f0b21bb28c55c1cc38af830c762eede895a6936d
Michael Walter <mcoywalter@gmail.com>
2021-03-04 14:33:50 +0100

    OPLSlmp calcúlator introduced

---
+++ ase/calculators/oplslmp.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+from ase.io.opls import OPLSff
+from ase.calculators.lammpsrun import LAMMPS
+
+
+class OPLSlmp(LAMMPS):
+    implemented_properties = ['energy', 'energies', 'forces']
+
+    def __init__(self, parfile, **kwargs):
+        """
+        parfile: string
+           OPLS parameter file name
+        """
+        self.oplsff = OPLSff(open(parfile))
+        kwargs['tmp_dir'] = 'oplslmp'
+        kwargs['keep_tmp_files'] = True
+
+        super().__init__(**kwargs)
+
+        # change defaults to be suitable with OPLS
+        self.parameters.update(dict(
+            atom_style='full',
+        ))
+        
+    def _write_lammps_data(self, label, tempdir):
+        prefix = str(Path(tempdir) / label)
+        self.parameters.update({
+            'kspace_modify': 'gewald',
+            'interactions_file': prefix + '_opls'})
+        return self.oplsff.write_lammps(self.atoms, prefix=prefix)
