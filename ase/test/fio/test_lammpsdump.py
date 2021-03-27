b4c7a75a32de8f8e93b4227d48787f6fbde4f03b
T. K. Stenczel <tks32@cam.ac.uk>
2021-03-27 10:11:24 +0000

    TST: add test for reading elements from lammpsdump

--- ase/test/fio/test_lammpsdump.py
+++ ase/test/fio/test_lammpsdump.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pytest
+
 from ase.io.formats import ioformats, match_magic
 
 
@@ -44,3 +45,26 @@ ITEM: ATOMS id type xu yu zu
     assert atoms.cell.orthorhombic
     assert pytest.approx(atoms.cell.lengths()) == [4., 5., 20.]
     assert pytest.approx(atoms.positions) == ref_positions[order - 1]
+
+
+def test_lammpsdump_element():
+    # Test lammpsdump with elements column given
+    buf = """\
+ITEM: TIMESTEP
+0
+ITEM: NUMBER OF ATOMS
+3
+ITEM: BOX BOUNDS pp pp pp
+0.0e+00 4e+00
+0.0e+00 5.0e+00
+0.0e+00 2.0e+01
+ITEM: ATOMS element type xu yu zu
+H 1 1.2 1.3 1.4
+H 1 2.3 2.4 2.5
+He 1 3.4 3.5 3.6
+"""
+
+    fmt = ioformats['lammps-dump-text']
+    atoms = fmt.parse_atoms(buf)
+
+    assert np.all(atoms.get_atomic_numbers() == np.array([1, 1, 2]))
