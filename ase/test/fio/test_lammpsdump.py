77d83d573c45dc864dbf613009ca28e2111bc976
T. K. Stenczel <tks32@cam.ac.uk>
2021-03-29 10:16:07 +0100

    TST: simplify substitution in lammps fixture

--- ase/test/fio/test_lammpsdump.py
+++ ase/test/fio/test_lammpsdump.py
@@ -35,28 +35,25 @@ def lammpsdump():
                 have_element=True,
                 have_id=False,
                 have_type=True):
-        buf = """\
-            ITEM: TIMESTEP
-            0
-            ITEM: NUMBER OF ATOMS
-            3
-            ITEM: BOX BOUNDS _bounds_
-            0.0e+00 4e+00
-            0.0e+00 5.0e+00
-            0.0e+00 2.0e+01
-            ITEM: ATOMS _element_ _id_ _type_ _pos_cols_
-            C  1 1 0.5 0.6 0.7
-            C  3 1 0.6 0.1 1.9
-            Si 2 2 0.45 0.32 0.67
-            """
-        for field, val in [
-            ("_bounds_", bounds),
-            ("_pos_cols_", position_cols),
-            ("_element_", "element" if have_element else "unk0"),
-            ("_id_", "id" if have_id else "unk1"),
-            ("_type_", "type" if have_type else "unk2"),
-        ]:
-            buf = buf.replace(field, val)
+
+        _element = "element" if have_element else "unk0"
+        _id = "id" if have_id else "unk1"
+        _type = "type" if have_type else "unk2"
+
+        buf = f"""\
+        ITEM: TIMESTEP
+        0
+        ITEM: NUMBER OF ATOMS
+        3
+        ITEM: BOX BOUNDS {bounds}
+        0.0e+00 4e+00
+        0.0e+00 5.0e+00
+        0.0e+00 2.0e+01
+        ITEM: ATOMS {_element} {_id} {_type} {position_cols}
+        C  1 1 0.5 0.6 0.7
+        C  3 1 0.6 0.1 1.9
+        Si 2 2 0.45 0.32 0.67
+        """
 
         return buf
 
