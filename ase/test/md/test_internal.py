ad81c3bd59bf7bd4e0abff3d76138298b23b59e8
Michael Walter <mcoywalter@gmail.com>
2021-04-12 16:15:26 +0200

    Allow for internal md and test

---
+++ ase/test/md/test_internal.py
@@ -0,0 +1,40 @@
+import pytest
+
+from ase.units import fs, kB
+from ase.build import molecule
+from ase.constraints import FixAtoms
+from ase.md.internal import Internal
+from ase.io import Trajectory
+
+
+@pytest.mark.calculator_lite
+@pytest.mark.calculator('dftb')
+def test_dftb(factory):
+    atoms = molecule('H2O')
+    initial_positions = atoms.get_positions().copy()
+    
+    fixed = 0
+    atoms.set_constraint(FixAtoms([fixed]))
+
+    atoms.calc = factory.calc(label='h2o/main')
+    
+    timestep = 2 * fs
+    trajname = 'h2o.traj'
+    dyn = Internal(atoms, timestep, 300 * kB, trajectory=trajname)
+
+    steps0 = 5
+    dyn.run(steps0)
+
+    traj = Trajectory('h2o.traj')
+    assert len(traj) == steps0
+
+    steps1 = 3
+    dyn.run(steps1)
+
+    traj = Trajectory('h2o.traj')
+    assert len(traj) == steps0 + steps1
+
+    for atoms in traj:
+        assert atoms.calc.name == 'dftb'
+        assert atoms[fixed].position == pytest.approx(
+            initial_positions[fixed], 1e-6)
