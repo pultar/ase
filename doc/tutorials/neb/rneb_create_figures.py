e3888798e38f6d45b2b60a0081588bd2102d808f
Steen Lysgaard <stly@dtu.dk>
2021-08-13 11:10:00 +0200

    pep8

--- doc/tutorials/neb/rneb_create_figures.py
+++ doc/tutorials/neb/rneb_create_figures.py
@@ -7,10 +7,10 @@ runpy.run_path('rneb_reflective.py')
 runpy.run_path('rneb_rmineb.py')
 
 images = read('neb.traj@-5:')
-for name, a in zip('IF', images[::len(images)-1]):
+for name, a in zip('IF', images[::len(images) - 1]):
     cell = a.get_cell()
     del a.constraints
     a = a * (2, 2, 1)
     a.set_cell(cell)
     write('rneb-%s.pov' % name, a,
-          povray_settings={'transparent':False, 'display':False})
+          povray_settings={'transparent': False, 'display': False})
