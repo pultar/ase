859f82be088b0a48667b4b2bcbf2a18d22bb8c89
Alexander Tygesen <atygesen@outlook.dk>
2022-02-15 10:12:59 +0100

    Some calculators may wish to adjust reset() behaviour

--- ase/calculators/calculator.py
+++ ase/calculators/calculator.py
@@ -496,8 +496,7 @@ class BaseCalculator(GetPropertiesMixin):
             system_changes = self.check_state(atoms)
 
             if system_changes:
-                self.atoms = None
-                self.results = {}
+                self.reset()
 
         if name not in self.results:
             if not allow_calculation:
@@ -538,6 +537,11 @@ class BaseCalculator(GetPropertiesMixin):
     @property
     def name(self) -> str:
         return self._get_name()
+    
+    def reset(self):
+        """Clear all information from old calculation."""
+        self.atoms = None
+        self.results = {}
 
 
 class Calculator(BaseCalculator):
@@ -725,12 +729,6 @@ class Calculator(BaseCalculator):
                     continue
             dct[key] = value
         return dct
-
-    def reset(self):
-        """Clear all information from old calculation."""
-
-        self.atoms = None
-        self.results = {}
 
     def read(self, label):
         """Read atoms, parameters and calculated properties from output file.
