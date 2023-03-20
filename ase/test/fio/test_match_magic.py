856fddf72e9786c892a2a07548afdd468ab7729f
Ask Hjorth Larsen <asklarsen@gmail.com>
2023-03-20 15:15:47 +0100

    clean up the gpaw match magic test

--- ase/test/fio/test_match_magic.py
+++ ase/test/fio/test_match_magic.py
@@ -1,17 +1,17 @@
-# flake8: noqa
+from ase.io.formats import ioformats
 
-def test_match_magic():
-    from ase.io.formats import ioformats
 
-    text = b"""
+text = b"""
 
-      ___ ___ ___ _ _ _  
-     |   |   |_  | | | | 
-     | | | | | . | | | | 
-     |__ |  _|___|_____|  19.8.2b1
-     |___|_|             
+  ___ ___ ___ _ _ _  
+ |   |   |_  | | | | 
+ | | | | | . | | | | 
+ |__ |  _|___|_____|  19.8.2b1
+ |___|_|             
 
-    """
+"""
 
+
+def test_gpaw_match_magic():
     gpaw = ioformats['gpaw-out']
     assert gpaw.match_magic(text)
