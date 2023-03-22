8343609ad3fe3a02d0216140c1848d96e09d4438
Adam J. Jackson <a.j.jackson@physics.org>
2023-03-22 18:29:47 +0100

    Simplify typing of WeightedKPoints
    Having a hard time satisfying MyPy with this classmethod stuff, just
    give the actual type and move on.

--- ase/calculators/calculator.py
+++ ase/calculators/calculator.py
@@ -5,9 +5,8 @@ import warnings
 
 from abc import abstractmethod
 from math import pi, sqrt
-import numbers
 from pathlib import Path
-from typing import Any, Dict, List, Optional, Set, Sequence, TypeVar, Union
+from typing import Any, Dict, List, Optional, Set, Union
 
 import numpy as np
 
@@ -369,13 +368,10 @@ class KPoints(KPointsABC):
         return vars(self)
 
 
-WK = TypeVar('WK', bound='WeightedKPoints')
-
-
 class WeightedKPoints(KPoints):
     def __init__(self,
                  kpts: np.ndarray,
-                 weights: Sequence[numbers.Real]) -> None:
+                 weights: np.ndarray) -> None:
         self.weights = weights
         super().__init__(kpts=kpts)
 
@@ -383,7 +379,7 @@ class WeightedKPoints(KPoints):
             raise IndexError("Number of weights does not match number of kpts")
 
     @classmethod
-    def from_array(cls: WK, data: np.ndarray) -> WK:
+    def from_array(cls, data: np.ndarray) -> 'WeightedKPoints':
         """Construct k-points with weights from Nx4 array"""
         if data.shape[1] == 4:
             return cls(kpts=data[:, :3], weights=data[:, 3])
