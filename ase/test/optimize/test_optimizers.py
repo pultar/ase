0391bb03e0373f3a14657f7fe81bec04315dee5c
Ugochukwu Nwosu <ugognw@gmail.com>
2023-06-27 18:16:12 -0600

    TST: Refactor Optimizer tests for clarity
    Moving code required to set up tests into pytest fixtures improves the
    readability of the test by minimizing boilerplate code. Additionally,
    doing so facilitates the sharing of resources between tests and
    simplifies parametrization of tests with respect to certain inputs
    (e.g, Optimizer subclass or step limit).
    
    Signed-off-by: Ugochukwu Nwosu <ugognw@gmail.com>

--- ase/test/optimize/test_optimizers.py
+++ ase/test/optimize/test_optimizers.py
@@ -1,15 +1,24 @@
 from functools import partial
-from itertools import product
 
 import pytest
-
 from ase.build import bulk
 from ase.calculators.emt import EMT
-from ase.optimize import (MDMin, FIRE, LBFGS, LBFGSLineSearch, BFGSLineSearch,
-                          BFGS, GoodOldQuasiNewton, GPMin, Berny, ODE12r)
-from ase.optimize.sciopt import OptimizerConvergenceError, SciPyFminCG, SciPyFminBFGS
+from ase.cluster import Icosahedron
+from ase.optimize import (
+    BFGS,
+    FIRE,
+    LBFGS,
+    Berny,
+    BFGSLineSearch,
+    GoodOldQuasiNewton,
+    GPMin,
+    LBFGSLineSearch,
+    MDMin,
+    ODE12r,
+)
+from ase.optimize.optimize import Optimizer
 from ase.optimize.precon import PreconFIRE, PreconLBFGS, PreconODE12r
-from ase.optimize.sciopt import SciPyFminBFGS, SciPyFminCG
+from ase.optimize.sciopt import OptimizerConvergenceError, SciPyFminBFGS, SciPyFminCG
 
 optclasses = [
     MDMin,
@@ -30,24 +39,26 @@ optclasses = [
 ]
 
 
-@pytest.fixture(name="ref_atoms")
+@pytest.fixture(name="ref_atoms", scope="module")
 def fixture_ref_atoms(optcls):
     if optcls is Berny:
         ref_atoms = Icosahedron("Ag", 2, 3.82975)
         ref_atoms.calc = EMT()
         return ref_atoms
 
-    ref_atoms = bulk("Au")
-    ref_atoms.calc = EMT()
-    ref_atoms.get_potential_energy()
-    return ref_atoms
+    atoms = bulk("Au")
+    atoms.calc = EMT()
+    atoms.get_potential_energy()
+    return atoms
 
 
-@pytest.fixture(name="atoms")
+@pytest.fixture(name="atoms", scope="module")
 def fixture_atoms(ref_atoms, optcls):
     if optcls is Berny:
         atoms = ref_atoms.copy()
         floor = 7
+        optcls = partial(optcls, dihedral=False)
+        optcls.__name__ = Berny.__name__
     else:
         atoms = ref_atoms * (2, 2, 2)
         floor = 0.45
@@ -56,45 +67,55 @@ def fixture_atoms(ref_atoms, optcls):
     atoms.rattle(stdev=0.1, seed=7)
     e_unopt = atoms.get_potential_energy()
     assert e_unopt > floor
+    assert e_unopt > floor
     return atoms
 
 
-@pytest.fixture(name="optcls", params=optclasses)
+@pytest.fixture(name="optcls", scope="module", params=optclasses)
 def fixture_optcls(request):
     optcls = request.param
     if optcls is Berny:
         pytest.importorskip("berny")  # check if pyberny installed
+
     return optcls
 
 
-@pytest.fixture(name="kwargs")
+@pytest.fixture(name="to_catch", scope="module")
+def fixture_to_catch(optcls):
+    if optcls in (ODE12r, PreconODE12r):
+        return (OptimizerConvergenceError,)
+    return []
+
+
+@pytest.fixture(name="kwargs", scope="module")
 def fixture_kwargs(optcls):
     kwargs = {}
     if optcls is PreconLBFGS:
         kwargs["precon"] = None
-    if optcls is Berny:
-        kwargs["dihedral"] = False
     yield kwargs
     kwargs = {}
 
 
 @pytest.mark.optimize
 @pytest.mark.filterwarnings("ignore: estimate_mu")
-def test_optimize(optcls, atoms, ref_atoms, kwargs):
-    """Test if forces can be converged using the optimizer."""
+def test_optimize(optcls, atoms, ref_atoms, testdir, kwargs):
     fmax = 0.01
-    with optcls(atoms, **kwargs) as opt:
+    with optcls(atoms, logfile=testdir / "opt.log", **kwargs) as opt:
         is_converged = opt.run(fmax=fmax)
     assert is_converged  # check if opt.run() returns True when converged
 
     forces = atoms.get_forces()
     final_fmax = max((forces**2).sum(axis=1) ** 0.5)
+    final_fmax = max((forces**2).sum(axis=1) ** 0.5)
     ref_energy = ref_atoms.get_potential_energy()
     e_opt = atoms.get_potential_energy() * len(ref_atoms) / len(atoms)
     e_err = abs(e_opt - ref_energy)
-
-    print(f"{optcls.__name__:>20}:", end=" ")
-    print(f"fmax={final_fmax:.05f} eopt={e_opt:.06f} err={e_err:06e}")
+    print()
+    print(
+        "{:>20}: fmax={:.05f} eopt={:.06f}, err={:06e}".format(
+            optcls.__name__, final_fmax, e_opt, e_err
+        )
+    )
 
     assert final_fmax < fmax
     assert e_err < 1.75e-5  # (This tolerance is arbitrary)
@@ -122,26 +143,10 @@ def test_run_twice(optcls, atoms, kwargs):
 
 @pytest.mark.parametrize('optcls,max_steps', product(optclasses, range(-1,2)))
 def test_should_respect_steps_limit(
-    optcls, atoms, max_steps, testdir
+    optcls: Optimizer, max_steps: int, atoms, testdir, kwargs, to_catch
 ):
-    if optcls is Berny:
-        pytest.importorskip('berny')  # check if pyberny installed
-        optcls = partial(optcls, dihedral=False)
-        optcls.__name__ = Berny.__name__
-        atoms, _ = atoms_no_pbc()
-    kw = {}
-    if optcls is PreconLBFGS:
-        kw['precon'] = None
-    else:
-        kw = {}
-    
-    if optcls in (ODE12r, PreconODE12r):
-        to_catch = (OptimizerConvergenceError,)
-    else:
-        to_catch = []
-
     fmax = 0.01
-    with optcls(atoms, logfile=testdir / "opt.log", **kw) as opt:
+    with optcls(atoms, logfile=testdir / "opt.log", **kwargs) as opt:
         try:
             opt.run(fmax=fmax, steps=max_steps)
         except to_catch:
