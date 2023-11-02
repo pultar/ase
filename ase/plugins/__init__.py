6647d0ec301d8d5856d327d598573efa0ddc9a28
Matyas Novak <logik@centrum.cz>
2023-11-02 11:03:58 +0100

    Builtin plugins is named builtins

--- ase/plugins/__init__.py
+++ ase/plugins/__init__.py
@@ -1,7 +1,7 @@
 """Atomic Simulation Environment plugin package."""
 __path__ = __import__('pkgutil').extend_path(__path__, __name__)
 
-from .ase.plugins import Plugins
+from .builtins.plugins import Plugins
 
 plugins = Plugins('ase.plugins')
 calculators = plugins.calculators
