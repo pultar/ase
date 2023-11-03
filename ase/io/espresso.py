c0c0bb0dc8e92502a483b29798e71eee7656155e
Pietro Delugas <mail@pietrodelugas.it>
2023-11-03 12:00:30 +0100

    fix flake again

--- ase/io/espresso.py
+++ ase/io/espresso.py
@@ -1317,8 +1317,8 @@ def write_espresso_in(fd, atoms, input_data=None, pseudopotentials=None,
 
     - Conversion of :class:`ase.constraints.FixAtoms` and
                     :class:`ase.constraints.FixCartesian`.
-    - `starting_magnetization` derived from the `magmoms` and pseudopontial valences
-       given in input
+    - `starting_magnetization` derived from the `magmoms` and
+       pseudopontial valences given in input
       (searches default paths for pseudo files.)
     - Automatic assignment of options to their correct sections.
 
@@ -1342,8 +1342,7 @@ def write_espresso_in(fd, atoms, input_data=None, pseudopotentials=None,
     pseudopotentials: dict
         A filename for each atomic species, e.g.
         {'O': 'O.pbe-rrkjus.UPF', 'H': 'H.pbe-rrkjus.UPF'}.
-        or 
-        a dict for each atomic species with at least a filename key, 
+        or a dict for each atomic species with at least a filename key,
         other parsed keys are:
         valence, cutoff_wfc, cutoff_rho, e.g.
         {'O':{'filename': 'O.pbe-rrkjus.UPF',
@@ -1405,41 +1404,40 @@ def write_espresso_in(fd, atoms, input_data=None, pseudopotentials=None,
             mask = ''
         masks.append(mask)
 
-    # Species info holds the information on the pseudopotential 
+    # Species info holds the information on the pseudopotential
     # associated for each element
     if pseudopotentials is None:
         pseudopotentials = {}
     species_info = {}
     for species in set(atoms.get_chemical_symbols()):
-        znum  = atomic_symbols.atomic_numbers[species]
+        znum = atomic_symbols.atomic_numbers[species]
         # Look in all possible locations for the pseudos and try to figure
         # out the number of valence electrons
         pseudo = pseudopotentials.get(species, None)
         if isinstance(pseudo, str):
             species_info[species] = {'pseudo': pseudo,
-                                     'valence':SSSP_VALENCE[znum]}
+                                     'valence': SSSP_VALENCE[znum]}
         elif isinstance(pseudo, dict):
-            species_info[species] ={'pseudo': pseudo.get('filename', None),
-                                    'valence': pseudo.get('valence', 
-                                                          SSSP_VALENCE[znum]),
-                                     'cutoff_wfc': pseudo.get('cutoff_wfc',0),
-                                     'cutoff_rho': pseudo.get('cutoff_rho',0),                     
-                                    }
+            species_info[species] = {'pseudo': pseudo.get('filename', None),
+                                     'valence': pseudo.get('valence',
+                                                           SSSP_VALENCE[znum]),
+                                     'cutoff_wfc': pseudo.get('cutoff_wfc', 0),
+                                     'cutoff_rho': pseudo.get('cutoff_rho', 0)
+                                     }
 
     # Convert atoms into species.
     # Each different magnetic moment needs to be a separate type even with
     # the same pseudopotential (e.g. an up and a down for AFM).
     # if any magmom are > 0 or nspin == 2 then use species labels.
     # Rememeber: magnetisation uses 1 based indexes
-    atomic_species = OrderedDict()
     atomic_species_str = []
     atomic_positions_str = []
     nspin = input_parameters['system'].get('nspin', 1)  # 1 is the default
     noncolin = input_parameters['system'].get('noncolin', False)
     rescale_magmom = kwargs.get('rescale_magmom', False)
-    on_site_hubbard = kwargs.get('on_site_hubbard', None) 
+    on_site_hubbard = kwargs.get('on_site_hubbard', None)
     if any(atoms.get_initial_magnetic_moments()):
-        if  nspin == 1 and not noncolin:
+        if nspin == 1 and not noncolin:
             # Force spin on
             input_parameters['system']['nspin'] = 2
             nspin = 2
@@ -1447,35 +1445,37 @@ def write_espresso_in(fd, atoms, input_data=None, pseudopotentials=None,
     if nspin == 2 or noncolin:
         if on_site_hubbard is not None:
             initial_magmom = split_nonequivalent_hubbard(
-                atoms, atoms.get_initial_magnetic_moments(), on_site_hubbard
-                )
+                atoms, atoms.get_initial_magnetic_moments(), on_site_hubbard)
         else:
-            initial_magmom = atoms.get_initial_magnetic_moments() 
+            initial_magmom = atoms.get_initial_magnetic_moments()
         # Magnetic calculation on: split with magmom
             atomic_species_str, atomic_positions_str = get_split_atomic_cards(
-            atoms, masks, initial_magmom, species_info, crystal_coordinates)
+                atoms, masks, initial_magmom, species_info, crystal_coordinates)
             for pwspecies in [_.split()[0] for _ in atomic_species_str]:
-                posidx = [_.split()[0] for _ in atomic_positions_str].index(pwspecies)
-                spidx  = [_.split()[0] for _ in atomic_species_str].index(pwspecies) 
+                posidx = [
+                    _.split()[0] for _ in atomic_positions_str].index(pwspecies)
+                spidx = [
+                    _.split()[0] for _ in atomic_species_str].index(pwspecies)
                 fspin = atoms.get_initial_magnetic_moments()[posidx]
                 mag_str = f"starting_magnetization({spidx + 1})"
-                # QE v7.2 and older need magnetization between -1 and +1, initial
-                # magnetic moments are rescaled with pseudopotential valence charge
-                # for QE newer that 7.2 it is non necessary to set rescale_magnom=True
-                # but still works 
+                # QE v7.2 and older need magnetization between -1 and +1,
+                # initial magnetic moments are rescaled with pseudopotential
+                #  valence charge for QE newer that 7.2 it is non necessary to
+                # set rescale_magnom = True but still works
                 if rescale_magmom:
-                    fspin = fspin / species_info[atoms.symbols[posidx]]['valence']
+                    fspin = fspin / species_info[
+                        atoms.symbols[posidx]]['valence']
                 input_parameters['system'][mag_str] = fspin
     else:
         # Do nothing about magnetisation and split using hubbard labels if any
-        on_site_hubbard_ = [None]*len(atoms) if on_site_hubbard is None else on_site_hubbard
+        on_site_hubbard_ = (
+            [None] * len(atoms) if on_site_hubbard is None else on_site_hubbard
+        )
         atomic_species_str, atomic_positions_str = get_split_atomic_cards(
             atoms, masks, on_site_hubbard, species_info, crystal_coordinates)
-    hubbard_cards_str = onsite_hubbard_card(
-        on_site_hubbard_, atomic_positions_str
-        ) if on_site_hubbard is not None else None 
-        
-
+    hubbard_cards_str = (
+        onsite_hubbard_card(on_site_hubbard_, atomic_positions_str)
+        if on_site_hubbard is not None else None)
 
     # different magnetisms means different types
     input_parameters['system']['ntyp'] = len(atomic_species_str)
@@ -1567,10 +1567,10 @@ def write_espresso_in(fd, atoms, input_data=None, pseudopotentials=None,
     pwi.extend(atomic_positions_str)
     pwi.append('\n')
     if hubbard_cards_str is not None:
-        hubbard_projection = kwargs.get('hubbard_projections',"ortho-atomic")
+        hubbard_projection = kwargs.get('hubbard_projections', "ortho-atomic")
         pwi.append(f"Hubbard ({hubbard_projection})\n")
         pwi.extend(hubbard_cards_str)
-        pwi.append('\n') 
+        pwi.append('\n')
     # DONE!
     fd.write(''.join(pwi))
 
@@ -1579,46 +1579,50 @@ def split_nonequivalent_hubbard(atoms, magmom, onsite_hubbard):
     """
     assigns neglibly different magmoms to atoms that in input have
     same symbol. same magmom but different onsite hubbard terms. This helps
-    assigning different labeled species in the case magnetic calculations with 
-    on site hubbard correction. 
+    assigning different labeled species in the case magnetic calculations with
+    on site hubbard correction.
     Parameters:
     ----------------------------------------------
-    atoms: ase.Atoms 
+    atoms: ase.Atoms
     magmom: list of floats
             list of the magnetic moments from  atoms
-    onsite_hubbard: tuple 
+    onsite_hubbard: tuple
                     with hubbard parameters for each of the atoms site
     ------------------------------------------------
     Returns:
-    magmoms_: list of floats 
-              the neglibly changed magnetic moments 
+    magmoms_: list of floats
+              the neglibly changed magnetic moments
     """
     magmom_ = [_ for _ in magmom]
     count = 0
-    sites = OrderedDict() 
+    sites = OrderedDict()
     for atom in zip([a.symbol for a in atoms], magmom_, onsite_hubbard):
-        sites[tuple(atom)] = sites.get(tuple(atom),[]) + [count,]
-        count +=1 
+        sites[tuple(atom)] = sites.get(tuple(atom), []) + [count, ]
+        count += 1
     for s in enumerate(sites):
         for s2 in enumerate(sites):
-            if s2[0]>s[0]:
+            if s2[0] > s[0]:
                 if magmom_[sites[s2[1]][0]] == magmom_[sites[s[1]][0]]:
                     for index in sites[s2[1]]:
-                        magmom_[index] += 1.e-4*s2[0]*np.sign(magmom_[index])
-    return magmom_	
+                        magmom_[index] += 1.e-4 * s2[0] * (
+                            np.sign(magmom_[index]))
+    return magmom_
 
 
-
-def get_split_atomic_cards(atoms, masks, sites, species_info, crystal_coordinates):
+def get_split_atomic_cards(atoms, masks, sites, species_info,
+                           crystal_coordinates):
     """
-    generates different species labels for atoms with the same symbol but different site properties. 
+    generates different species labels for atoms with the same symbol but
+    different site properties.
     Parameters
     ------------------------------------------------
-    atoms: ase.Atoms 
+    atoms: ase.Atoms
     masks: str describes atomic constraints for relaxation or dynamics
-    sites: list same len as atoms, contains the info of the site specific property
-    species_info: dict 
-    crystal_coordinates: bool if True atoms will be written in crystal coordinates 
+    sites: list same len as atoms, contains the info of the site specific
+    property
+    species_info: dict
+    crystal_coordinates: bool if True atoms will be written in crystal
+    coordinates
     """
     atomic_species_str = []
     atomic_positions_str = []
@@ -1627,43 +1631,41 @@ def get_split_atomic_cards(atoms, masks, sites, species_info, crystal_coordinate
         try:
             pseudo_ = species_info[atom.symbol]['pseudo']
         except KeyError:
-            pseudo_ = "pseudo_not_provided" 
+            pseudo_ = "pseudo_not_provided"
         if (atom.symbol, site) not in atomic_species_:
-			#index in atomic species list
+            # index in atomic species list
             sidx = len(atomic_species_) + 1
-			#index for that atomic type no index for first
+            # index for that atomic type no index for first
             tidx = sum(atom.symbol == x[0] for x in atomic_species_) or ' '
             atomic_species_[atom.symbol, site] = (sidx, tidx)
             atomic_species_str.append(
-				f"{atom.symbol}{tidx}  {atom.mass}  {pseudo_}\n"
-			)
-		#lookup tidx to append to name
+                f"{atom.symbol}{tidx}  {atom.mass}  {pseudo_}\n")
+        # lookup tidx to append to name
         (sidx, tidx) = atomic_species_[atom.symbol, site]
         if crystal_coordinates:
             coords = [atom.a, atom.b, atom.c]
         else:
             coords = atom.position
-        coords_str = "".join([f"{coords[i]:.10f} " for i in range(3)]) 
+        coords_str = "".join([f"{coords[i]:.10f} " for i in range(3)])
         atomic_positions_str.append(
-			f"{atom.symbol}{tidx}  "  f"{coords_str}" f"{mask}\n"
-		)
+            f"{atom.symbol}{tidx}  "  f"{coords_str}" f"{mask}\n"
+        )
     return atomic_species_str, atomic_positions_str
 
-	
+
 def onsite_hubbard_card(onsite_hubbard, atomic_positions_str):
     Udata = filter(
-        lambda s: s[0][0] is not None, 
+        lambda s: s[0][0] is not None,
         filter(lambda s: s[0] is not None,
-        zip(onsite_hubbard,atomic_positions_str))
-        )
+               zip(onsite_hubbard, atomic_positions_str))
+    )
     U_str = [f"U  {s[1].split()[0]}-{s[0][2]} {s[0][0]:.3f}\n" for s in Udata]
     Jdata = filter(
-		lambda s: s[0][1] is not None, 
+        lambda s: s[0][1] is not None,
         filter(lambda s: s[0] is not None,
-        zip(onsite_hubbard,atomic_positions_str))
-        )
+               zip(onsite_hubbard, atomic_positions_str))
+    )
     J_str = [f"J  {s[1].split()[0]}-{s[0][2]} {s[0][1]:.3f}\n" for s in Jdata]
     res = list(set(U_str + J_str))
     res = res if len(res) > 0 else None
-    return res  
-	
+    return res
