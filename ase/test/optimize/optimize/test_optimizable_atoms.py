f65d733cc5c989d8332884d0d3d86c3c74d0cc96
Ugochukwu Nwosu <ugognw@gmail.com>
2023-11-03 20:02:04 -0600

    Fix tests
    Signed-off-by: Ugochukwu Nwosu <ugognw@gmail.com>

--- ase/test/optimize/optimize/test_optimizable_atoms.py
+++ ase/test/optimize/optimize/test_optimizable_atoms.py
@@ -12,7 +12,7 @@ def fixture_optimizable_atoms(atoms: Atoms) -> OptimizableAtoms:
 class TestOptimizableAtoms:
     @staticmethod
     def test_should_get_positions(optimizable_atoms: OptimizableAtoms, atoms: Atoms):
-        assert optimizable_atoms.get_positions() == atoms.get_positions()
+        assert (optimizable_atoms.get_positions() == atoms.get_positions()).all()
 
     @staticmethod
     def test_should_set_positions(optimizable_atoms: OptimizableAtoms):
@@ -20,23 +20,19 @@ class TestOptimizableAtoms:
         copied_optimizable = OptimizableAtoms(copied_atoms)
         new_positions = copied_optimizable.get_positions() + [1, 1, 1]
         copied_optimizable.set_positions(new_positions)
-        assert copied_optimizable.get_positions() == new_positions
+        assert (copied_optimizable.get_positions() == new_positions).all()
 
     @staticmethod
     def test_should_get_forces(optimizable_atoms: OptimizableAtoms, atoms: Atoms):
-        assert optimizable_atoms.get_forces() == atoms.get_forces()
+        assert (optimizable_atoms.get_forces() == atoms.get_forces()).all()
 
     @staticmethod
     def test_should_get_potential_energy(optimizable_atoms: OptimizableAtoms, atoms: Atoms):
-        assert optimizable_atoms.get_potential_energy() == atoms.get_positions()
+        assert optimizable_atoms.get_potential_energy() == atoms.get_potential_energy()
 
     @staticmethod
     def test_should_iterimages(optimizable_atoms: OptimizableAtoms, atoms: Atoms):
-        res = []
-        for i, image in enumerate(optimizable_atoms.iterimages()):
-            res.append(image == atoms[i])
-
-        assert all(res)
+        assert next(optimizable_atoms.iterimages()) == atoms
 
     @staticmethod
     def test_should_get_chemical_symbols(optimizable_atoms: OptimizableAtoms, atoms: Atoms):
