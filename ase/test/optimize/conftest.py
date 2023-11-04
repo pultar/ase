9ac788b02d271f3c9aff6c6f115fdff1ef7b8b15
Ugochukwu Nwosu <ugognw@gmail.com>
2023-11-03 19:48:20 -0600

    Create common conftest for optimizer tests
    Signed-off-by: Ugochukwu Nwosu <ugognw@gmail.com>

--- ase/test/optimize/conftest.py
+++ ase/test/optimize/conftest.py
@@ -1,5 +1,23 @@
 import pytest
 
+from ase import Atoms
+from ase.build import bulk, molecule
+from ase.calculators.emt import EMT
 
-@pytest.fixture(name="atoms")
-def 
+
+ATOMS = [
+    bulk("Au"),
+    molecule("CO"),
+]
+
+@pytest.fixture(name="atoms", params=ATOMS)
+def fixture_atoms(request: pytest.FixtureRequest) -> Atoms:
+    atoms: Atoms = request.param
+    atoms.calc = EMT()
+    _ = atoms.get_potential_energy()
+    return atoms
+
+
+# atoms, NEB, filter, dimer
+@pytest.fixture(name="optimizable")
+def fixture_optimizable():...
