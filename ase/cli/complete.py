d42cafc7694437206fa68739457c78e0aa40c0bf
Matyas Novak <logik@centrum.cz>
2023-11-05 15:01:55 +0100

    ase info cmdline completing

--- ase/cli/complete.py
+++ ase/cli/complete.py
@@ -70,8 +70,8 @@ commands = {
          '-o', '--output', '-g', '--graph', '-t', '--terminal',
          '--interpolate', '-b', '--bonds', '-s', '--scale'],
     'info':
-        ['-v', '--verbose', '--formats', '--config', '--calculators',
-         '--plugins'],
+        ['-v', '--verbose', '--config', '--formats', '--calculators',
+         '--viewers', '--plugins'],
     'nebplot':
         ['--nimages', '--share-x', '--share-y'],
     'nomad-get':
