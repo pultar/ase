eb92bd85bd281e0f4e7b70f124f9a08e1ef2f021
Ugochukwu Nwosu <ugognw@gmail.com>
2023-11-05 15:02:37 -0700

    Update setup.py
    Signed-off-by: Ugochukwu Nwosu <ugognw@gmail.com>

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@
 # Please see the accompanying LICENSE file for further information.
 
 import os
-import re
 import sys
 from glob import glob
 from os.path import join
 
 from setuptools import find_packages, setup
 from setuptools.command.build_py import build_py as _build_py
+from setuptools_scm import ScmVersion
 
 python_min_version = (3, 8)
 python_requires = '>=' + '.'.join(str(num) for num in python_min_version)
@@ -36,7 +36,7 @@ extras_require = {
     'test': [
         'pytest>=6.2.5',
         'pytest-xdist>=2.1.0',
-    ]
+    ],
 }
 
 # Optional: spglib >= 1.9
@@ -45,22 +45,19 @@ extras_require = {
 with open('README.rst') as fd:
     long_description = fd.read()
 
-# Get the current version number:
-with open('ase/__init__.py') as fd:
-    version = re.search("__version__ = '(.*)'", fd.read()).group(1)
 
-
-package_data = {'ase': ['spacegroup/spacegroup.dat',
-                        'collections/*.json',
-                        'db/templates/*',
-                        'db/static/*',
-                        # make ASE a PEP 561 compliant package so that
-                        # other packages can use ASE's type hints:
-                        'py.typed'],
-                'ase.test': ['pytest.ini',
-                             'testdata/*',
-                             'testdata/*/*',
-                             'testdata/*/*/*']}
+package_data = {
+    'ase': [
+        'spacegroup/spacegroup.dat',
+        'collections/*.json',
+        'db/templates/*',
+        'db/static/*',
+        # make ASE a PEP 561 compliant package so that
+        # other packages can use ASE's type hints:
+        'py.typed',
+    ],
+    'ase.test': ['pytest.ini', 'testdata/*', 'testdata/*/*', 'testdata/*/*/*'],
+}
 
 
 class build_py(_build_py):
@@ -85,8 +82,9 @@ class build_py(_build_py):
                 mofile = join(dirname, 'ag.mo')
                 print()
                 print(f'Compile {pofile}')
-                status = os.system('%s -cv %s --output-file=%s 2>&1' %
-                                   (msgfmt, pofile, mofile))
+                status = os.system(
+                    '%s -cv %s --output-file=%s 2>&1' % (msgfmt, pofile, mofile)
+                )
                 assert status == 0, 'msgfmt failed!'
                 self.mofiles.append(mofile)
 
@@ -94,27 +92,38 @@ class build_py(_build_py):
         return _build_py.get_outputs(self, *args, **kwargs) + self.mofiles
 
 
-setup(name='ase',
-      version=version,
-      description='Atomic Simulation Environment',
-      url='https://wiki.fysik.dtu.dk/ase',
-      maintainer='ASE-community',
-      maintainer_email='ase-users@listserv.fysik.dtu.dk',
-      license='LGPLv2.1+',
-      platforms=['unix'],
-      packages=find_packages(),
-      python_requires=python_requires,
-      install_requires=install_requires,
-      extras_require=extras_require,
-      package_data=package_data,
-      entry_points={'console_scripts': ['ase=ase.cli.main:main']},
-      long_description=long_description,
-      cmdclass={'build_py': build_py},
-      classifiers=[
-          'Development Status :: 6 - Mature',
-          'License :: OSI Approved :: '
-          'GNU Lesser General Public License v2 or later (LGPLv2+)',
-          'Operating System :: OS Independent',
-          'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.8',
-          'Topic :: Scientific/Engineering :: Physics'])
+def set_version(version: ScmVersion) -> str:
+    from setuptools_scm.version import guess_next_version
+
+    # ! Add appropriate version specs
+
+    return version.format_next_version(guess_next_version, "{guessed}b{distance}")
+
+
+setup(
+    name='ase',
+    use_scm_version={"version_scheme": set_version},
+    description='Atomic Simulation Environment',
+    url='https://wiki.fysik.dtu.dk/ase',
+    maintainer='ASE-community',
+    maintainer_email='ase-users@listserv.fysik.dtu.dk',
+    license='LGPLv2.1+',
+    platforms=['unix'],
+    packages=find_packages(),
+    python_requires=python_requires,
+    install_requires=install_requires,
+    extras_require=extras_require,
+    package_data=package_data,
+    entry_points={'console_scripts': ['ase=ase.cli.main:main']},
+    long_description=long_description,
+    cmdclass={'build_py': build_py},
+    classifiers=[
+        'Development Status :: 6 - Mature',
+        'License :: OSI Approved :: '
+        'GNU Lesser General Public License v2 or later (LGPLv2+)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Scientific/Engineering :: Physics',
+    ],
+)
