2dc6b159d72d5bc96cef298514e3341d98a528cc
Matyas Novak <logik@centrum.cz>
2024-06-27 10:29:53 +0200

    [Plugins] Lint fix

--- ase/__init__.py
+++ ase/__init__.py
@@ -15,25 +15,5 @@ __version__ = '3.23.1b1'
 
 ase.parallel  # silence pyflakes
 
-"""
-This is a kind of magic, described here: `Packaging namespace packages
-<https://packaging.python.org/en/latest/guides/packaging-namespace-packages/>`
-We use a pkgutil-style namespace () package here, to allow use __init__.py.
-However, the plugins writers should use PEP420 native namespace package
-approach: they definitelly should NOT include __init__.py in ase and
-ase.plugins package.
-So the plugins structure should look like::
-ase/
-    plugins/
-            myplugin/
-                      __init.py__
-                      whatever_you_like.py
-
-The following command just tell to the python, that during the import it should
-look not only into the ase distribution package, but to the others distribution
-packages as well.
-Unfortunatelly (it's not documented well in the link above), it's not sufficient
-to make the ase.plugins package to be namespace package, we need to do it for
-all packages "on the way up" (so for the ase package as well).
-"""
-__path__ = __import__('pkgutil').extend_path(__path__, __name__)
+# To avoid circular import
+from . import plugins  # NOQA: E402, F401
