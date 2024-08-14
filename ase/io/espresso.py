28027a6e3e2d4a90262b366d70f866eabef43d6c
davide.bidoggia@outlook.it <davide.bidoggia@outlook.it>
2024-08-14 13:52:25 +0200

    Modified the fix for reading magmoms  using regex

--- ase/io/espresso.py
+++ ase/io/espresso.py
@@ -257,18 +257,11 @@ def read_espresso_out(fileobj, index=slice(None), results_required=True):
         magmoms = None
         for magmoms_index in indexes[_PW_MAGMOM]:
             if image_index < magmoms_index < next_index:
-                espresso_version = float(pwo_lines[image_index].split()[2][2:5])
-                if espresso_version < 6.8:
-                    magmoms = [
-                        float(mag_line.split()[5]) for mag_line
+                magmoms = [
+                        float(re.search(r"magn.\s+(-?\d+\.\d+)", mag_line).group(1)) for mag_line
                         in pwo_lines[magmoms_index + 1:
                                      magmoms_index + 1 + len(structure)]]
-                else:
-                    magmoms = [
-                        float(mag_line.split()[-1]) for mag_line
-                        in pwo_lines[magmoms_index + 1:
-                                    magmoms_index + 1 + len(structure)]]
-
+    
         # Dipole moment
         dipole = None
         if indexes[_PW_DIPOLE]:
